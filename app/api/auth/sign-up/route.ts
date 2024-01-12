938215274a4260774bd6d0cf824ea837baf2ac3e
Kamil Bazanow <kamil.bazanow@gmail.com>
2024-01-12 13:35:59 +0100

    refactor(/api/auth/sign-up): send password reset email if user already signed up

--- app/api/auth/sign-up/route.ts
+++ app/api/auth/sign-up/route.ts
@@ -5,9 +5,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import { emailSchema, passwordSchema } from '@/utils/validation';
 
 export async function POST(requset: NextRequest) {
-  const cookieStore = cookies();
-
   const { email, password } = await requset.json();
+  const cookieStore = cookies();
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -42,6 +41,21 @@ export async function POST(requset: NextRequest) {
     password,
   });
 
+  // * only for local development due to inconsistency error messages in signUp()
+  if (error?.message === 'User already registered') {
+    const redirectTo = requset.nextUrl.clone();
+    redirectTo.pathname = '/account/password-reset';
+
+    await supabase.auth.resetPasswordForEmail(email, {
+      redirectTo: redirectTo.href,
+    });
+
+    return NextResponse.json({
+      message:
+        'Welcome! To get started, please check your email and click the confirmation link. USER ALREADY EXIST',
+    });
+  }
+
   if (error) {
     return NextResponse.json(
       { error: error.message },
@@ -49,9 +63,32 @@ export async function POST(requset: NextRequest) {
     );
   }
 
+  // * switch with this in production
+  // if (error) {
+  //   return NextResponse.json(
+  //     { error: error.message },
+  //     { status: error.status },
+  //   );
+  // }
+
+  // if (data?.user?.identities?.length === 0) {
+  //   const redirectTo = requset.nextUrl.clone();
+  //   redirectTo.pathname = '/account/reset-password';
+
+  //   await supabase.auth.resetPasswordForEmail(email, {
+  //     redirectTo: redirectTo.href,
+  //   });
+
+  //   return NextResponse.json({
+  //     message:
+  //       'Welcome! To get started, please check your email and click the confirmation link. USER ALREADY EXIST',
+  //   });
+  // }
+
   return NextResponse.json(
     {
-      message: 'Succesfully created an account.',
+      message:
+        'Welcome! To get started, please check your email and click the confirmation link.',
     },
     { status: 200 },
   );
