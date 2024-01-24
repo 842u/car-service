3fe4dd1e82a3c40a89ab50088a7c72234e734385
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-01-24 14:14:10 +0100

    refactor(/api/auth/sign-up): remove trailing slash from redirect url

--- app/api/auth/sign-up/route.ts
+++ app/api/auth/sign-up/route.ts
@@ -41,7 +41,7 @@ export async function POST(requset: NextRequest) {
     email,
     password,
     options: {
-      emailRedirectTo: `${redirectUrl.origin}/`,
+      emailRedirectTo: redirectUrl.origin,
     },
   });
 
