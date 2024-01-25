1906164890173b0cdb8bd8dc743f370166e1c607
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-01-25 14:23:01 +0100

    refactor(AuthForm): use Input & SubmittButton

--- components/ui/AuthForm/AuthForm.tsx
+++ components/ui/AuthForm/AuthForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { twMerge } from 'tailwind-merge';
 
@@ -9,8 +9,8 @@ import {
   passwordValidationRules,
 } from '@/utils/validation';
 
-import { Spinner } from '../Spinner/Spinner';
-import { ToggleVisibilityButton } from '../ToggleVisibilityButton/ToggleVisibilityButton';
+import { Input } from '../Input/Input';
+import { SubmittButton } from '../SubmittButton/SubmittButton';
 
 type AuthFormValues = {
   email: string;
@@ -30,8 +30,6 @@ export default function AuthForm({
   strictPasswordCheck = true,
   className,
 }: AuthFormProps) {
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-
   const {
     register,
     handleSubmit,
@@ -44,10 +42,6 @@ export default function AuthForm({
       password: '',
     },
   });
-
-  const passwordVisibilityHandler = () => {
-    setIsPasswordVisible((currentState) => !currentState);
-  };
 
   const submitHandler: SubmitHandler<AuthFormValues> = async (data) => {
     const formData = JSON.stringify(data);
@@ -71,73 +65,37 @@ export default function AuthForm({
 
   return (
     <form
-      className={twMerge(
-        'flex flex-col items-center justify-center gap-4',
-        className,
-      )}
+      className={twMerge('flex flex-col', className)}
       onSubmit={handleSubmit(submitHandler)}
     >
-      <label className="w-full" htmlFor="email">
-        Email
-        <input
-          className={twMerge(
-            'mt-2 block w-full rounded-md border border-alpha-grey-500 bg-light-600 px-4 py-2 placeholder:text-sm placeholder:text-light-900 focus:border-alpha-grey-500 focus:ring-alpha-grey-700 dark:bg-dark-700 ',
-            errors.email?.message
-              ? 'border-error-500 bg-error-50 focus:border-error-500 dark:bg-error-900'
-              : '',
-          )}
-          {...register('email', emailValidationRules)}
-          id="email"
-          placeholder="Enter your email ..."
-          type="email"
-        />
-        <p className="my-2 whitespace-pre-wrap text-sm text-error-400">
-          {errors.email?.message || ' '}
-        </p>
-      </label>
-      <label className="w-full" htmlFor="password">
-        Password
-        <div className="relative">
-          <input
-            className={twMerge(
-              'mt-2 block w-full rounded-md border border-alpha-grey-500 bg-light-600 px-4 py-2 placeholder:text-sm placeholder:text-light-900 focus:border-alpha-grey-500 focus:ring-alpha-grey-700 dark:bg-dark-700 ',
-              errors.password?.message
-                ? 'border-error-500 bg-error-50 focus:border-error-500 dark:bg-error-900'
-                : '',
-            )}
-            {...register(
-              'password',
-              strictPasswordCheck
-                ? passwordValidationRules
-                : { required: 'This field is required' },
-            )}
-            id="password"
-            placeholder="Enter your password ..."
-            type={isPasswordVisible ? 'text' : 'password'}
-          />
-          <div className="absolute right-0 top-0 mr-2 flex h-full flex-col items-center justify-center">
-            <ToggleVisibilityButton
-              className="bg-alpha-grey-500 p-1"
-              isVisible={isPasswordVisible}
-              onClick={passwordVisibilityHandler}
-            />
-          </div>
-        </div>
-        <p className="my-2 whitespace-pre-wrap text-sm text-error-400">
-          {errors.password?.message || ' '}
-        </p>
-      </label>
-      <button
-        className="h-11 rounded-md border border-accent-500 bg-accent-800 px-4 py-2 text-light-500 transition-colors disabled:border-accent-700 disabled:bg-accent-900 disabled:text-light-800"
+      <Input
+        errorMessage={errors.email?.message}
+        label="Email"
+        name="email"
+        placeholder="Enter your email ..."
+        register={register}
+        registerOptions={emailValidationRules}
+        type="email"
+      />
+      <Input
+        errorMessage={errors.password?.message}
+        label="Password"
+        name="password"
+        placeholder="Enter your password ..."
+        register={register}
+        registerOptions={
+          strictPasswordCheck
+            ? passwordValidationRules
+            : { required: 'This field is required' }
+        }
+        type="password"
+      />
+      <SubmittButton
         disabled={!isValid || isSubmitting}
-        type="submit"
+        isSubmitting={isSubmitting}
       >
-        {isSubmitting ? (
-          <Spinner className="h-full" color="#88868c" />
-        ) : (
-          submitText
-        )}
-      </button>
+        {submitText}
+      </SubmittButton>
     </form>
   );
 }
