c6c7bfa93ae0322891a0ba4212489613c0be8a74
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-01-26 10:50:12 +0100

    test(ToggleVisibilityButton): add tests

--- components/ui/ToggleVisibilityButton/ToggleVisibilityButton.tsx
+++ components/ui/ToggleVisibilityButton/ToggleVisibilityButton.tsx
@@ -2,11 +2,11 @@ import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
 import { ComponentProps } from 'react';
 
 type ToggleVisibilityButtonProps = ComponentProps<'button'> & {
-  isVisible: boolean;
+  isVisible?: boolean;
 };
 
 export function ToggleVisibilityButton({
-  isVisible,
+  isVisible = true,
   ...props
 }: ToggleVisibilityButtonProps) {
   return (
@@ -17,9 +17,15 @@ export function ToggleVisibilityButton({
       className="w-6 rounded-md border border-alpha-grey-500 bg-alpha-grey-500 p-0.5"
     >
       {isVisible ? (
-        <EyeSlashIcon className="pointer-events-none aspect-square w-full" />
+        <EyeSlashIcon
+          className="pointer-events-none aspect-square w-full"
+          data-testid="eye-slash-icon"
+        />
       ) : (
-        <EyeIcon className="pointer-events-none aspect-square w-full" />
+        <EyeIcon
+          className="pointer-events-none aspect-square w-full"
+          data-testid="eye-icon"
+        />
       )}
     </button>
   );
