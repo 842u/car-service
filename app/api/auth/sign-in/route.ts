fa935a883b9427ea05b36fa86d14dbd9e2baf227
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-01-31 13:45:35 +0100

    refactor(/api/auth/sign-in): cleanup code

--- app/api/auth/sign-in/route.ts
+++ app/api/auth/sign-in/route.ts
@@ -1,33 +1,15 @@
-import { type CookieOptions, createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
 import { promiseWithTimeout } from '@/utils/general';
+import { getActionClient } from '@/utils/supabase';
 
 export async function POST(requset: NextRequest) {
   const { email, password } = await requset.json();
   const cookieStore = cookies();
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          cookieStore.set({ name, value, ...options });
-        },
-        remove(name: string, options: CookieOptions) {
-          cookieStore.set({ name, value: '', ...options });
-        },
-      },
-    },
-  );
-
+  const { auth } = getActionClient(cookieStore);
   const { error } = await promiseWithTimeout(
-    supabase.auth.signInWithPassword({
+    auth.signInWithPassword({
       email,
       password,
     }),
