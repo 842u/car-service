4276a79611e7c0f7642e7a3ca8af25f93271d1c2
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-01-31 13:43:29 +0100

    refactor(/api/auth/password-reset): cleanup code

--- app/api/auth/password-reset/route.ts
+++ app/api/auth/password-reset/route.ts
@@ -1,31 +1,12 @@
-import { type CookieOptions, createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
 import { promiseWithTimeout } from '@/utils/general';
+import { getActionClient } from '@/utils/supabase';
 import { passwordSchema } from '@/utils/validation';
 
 export async function PATCH(request: NextRequest) {
   const { password, passwordConfirm } = await request.json();
-  const cookieStore = cookies();
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          cookieStore.set({ name, value, ...options });
-        },
-        remove(name: string, options: CookieOptions) {
-          cookieStore.set({ name, value: '', ...options });
-        },
-      },
-    },
-  );
 
   try {
     passwordSchema.parse(password);
@@ -39,9 +20,9 @@ export async function PATCH(request: NextRequest) {
     );
   }
 
-  const { error } = await promiseWithTimeout(
-    supabase.auth.updateUser({ password }),
-  );
+  const cookieStore = cookies();
+  const { auth } = getActionClient(cookieStore);
+  const { error } = await promiseWithTimeout(auth.updateUser({ password }));
 
   if (error) {
     return NextResponse.json(
