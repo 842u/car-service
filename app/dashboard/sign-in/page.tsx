78c1dc569750d401d8441119cf4af690037eefce
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-01-31 16:28:51 +0100

    style(/dashboard/sign-in): add RWD

--- app/dashboard/sign-in/page.tsx
+++ app/dashboard/sign-in/page.tsx
@@ -1,25 +1,32 @@
 import Link from 'next/link';
 
+import { BrandLogoFull } from '@/components/decorative/BrandLogoFull';
 import AuthForm from '@/components/ui/AuthForm/AuthForm';
 
 export default async function SignInPage() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <h1 className="my-8 text-xl">Sign in to your accout.</h1>
-      <AuthForm
-        strictPasswordCheck={false}
-        submitText="Sign In"
-        submitUrl="/api/auth/sign-in"
-      />
-      <p className="my-4 text-sm text-light-800">
-        <span>Don&apos;t have an account? </span>
-        <Link
-          className="text-dark-500 underline dark:text-light-500"
-          href="/dashboard/sign-up"
-        >
-          Sign Up Now
-        </Link>
-      </p>
+    <main className="relative h-screen">
+      <div className="flex h-full w-full flex-col items-center justify-center bg-light-500 shadow-xl shadow-dark-700 transition-[background-color] dark:bg-dark-500 lg:w-2/5 lg:border-r lg:border-alpha-grey-500">
+        <h1 className="my-8 text-xl">Sign in to your accout.</h1>
+        <AuthForm
+          className="w-4/5 max-w-sm"
+          strictPasswordCheck={false}
+          submitText="Sign In"
+          submitUrl="/api/auth/sign-in"
+        />
+        <p className="my-4 text-sm text-light-800">
+          <span>Don&apos;t have an account? </span>
+          <Link
+            className="text-dark-500 underline dark:text-light-500"
+            href="/dashboard/sign-up"
+          >
+            Sign Up Now
+          </Link>
+        </p>
+      </div>
+      <div className="absolute left-0 top-0 -z-10 hidden h-full w-full bg-light-300 transition-[background-color] dark:bg-dark-700 lg:block">
+        <BrandLogoFull className="h-full w-full stroke-accent-500" />
+      </div>
     </main>
   );
 }
