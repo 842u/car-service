38633897b3cdf38fbb5113f9614c361c9aef87b3
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-02-28 11:35:03 +0100

    fix(security): properly set CSP

--- utils/security.mjs
+++ utils/security.mjs
@@ -1,66 +1,4 @@
-export function generateCspString(csp) {
-  const directives = Object.keys(csp);
-
-  const directivesString = directives.map(
-    (directive) => `${directive} ${csp[directive].join(' ')}`,
-  );
-
-  const cspString = directivesString.join('; ');
-
-  return cspString;
-}
-
-export const baseContentSecurityPolicy = {
-  'default-src': ["'self'"],
-  'base-uri': ["'self'"],
-  'connect-src': ["'self'"],
-  'script-src': [
-    "'self'",
-    "'sha256-eMuh8xiwcX72rRYNAGENurQBAcH7kLlAUQcoOri3BIo='",
-    "'strict-dynamic'",
-    `${process.env.DEVELOPMENT ? "'unsafe-eval'" : ''}`,
-  ],
-  'script-src-elem': [
-    "'self'",
-    "'sha256-eMuh8xiwcX72rRYNAGENurQBAcH7kLlAUQcoOri3BIo='",
-  ],
-  'style-src': [
-    "'self'",
-    "'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='",
-  ],
-  'font-src': ["'self'"],
-  'img-src': ["'self'", 'data:'],
-  'worker-src': ["'self'"],
-  'frame-ancestors': ["'none'"],
-  'form-action': ["'self'"],
-};
-
-export function generateCspWithNonce() {
-  const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
-
-  const contentSecurityPolicy = {
-    'script-src': [
-      ...baseContentSecurityPolicy['script-src'],
-      `'nonce-${nonce}'`,
-    ],
-    'script-src-elem': [
-      ...baseContentSecurityPolicy['script-src-elem'],
-      `'nonce-${nonce}'`,
-    ],
-    'style-src': [
-      ...baseContentSecurityPolicy['style-src'],
-      `'nonce-${nonce}'`,
-    ],
-  };
-
-  return { csp: generateCspString(contentSecurityPolicy), nonce };
-}
-
 export const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value: generateCspString(baseContentSecurityPolicy),
-  },
   {
     key: 'X-Frame-Options',
     value: 'SAMEORIGIN',
@@ -91,3 +29,62 @@ export const securityHeaders = [
       'camera=(), microphone=(), geolocation=() , accelerometer=(), gyroscope=(), magnetometer=(), payment=()',
   },
 ];
+
+export const baseContentSecurityPolicy = {
+  'default-src': ["'self'"],
+  'base-uri': ["'self'"],
+  'connect-src': ["'self'"],
+  'script-src': [
+    "'self'",
+    "'sha256-eMuh8xiwcX72rRYNAGENurQBAcH7kLlAUQcoOri3BIo='",
+    "'strict-dynamic'",
+    `${process.env.DEVELOPMENT ? "'unsafe-eval'" : ''}`,
+  ],
+  'script-src-elem': [
+    "'self'",
+    "'sha256-eMuh8xiwcX72rRYNAGENurQBAcH7kLlAUQcoOri3BIo='",
+  ],
+  'style-src': [
+    "'self'",
+    "'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='",
+  ],
+  'font-src': ["'self'"],
+  'img-src': ["'self'", 'data:'],
+  'worker-src': ["'self'"],
+  'frame-ancestors': ["'none'"],
+  'form-action': ["'self'"],
+};
+
+export function generateCspString(csp) {
+  const directives = Object.keys(csp);
+
+  const directivesString = directives.map(
+    (directive) => `${directive} ${csp[directive].join(' ')}`,
+  );
+
+  const cspString = directivesString.join('; ');
+
+  return cspString;
+}
+
+export function generateCspWithNonce() {
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
+
+  const contentSecurityPolicy = {
+    ...baseContentSecurityPolicy,
+    'script-src': [
+      ...baseContentSecurityPolicy['script-src'],
+      `'nonce-${nonce}'`,
+    ],
+    'script-src-elem': [
+      ...baseContentSecurityPolicy['script-src-elem'],
+      `'nonce-${nonce}'`,
+    ],
+    'style-src': [
+      ...baseContentSecurityPolicy['style-src'],
+      `'nonce-${nonce}'`,
+    ],
+  };
+
+  return { csp: generateCspString(contentSecurityPolicy), nonce };
+}
