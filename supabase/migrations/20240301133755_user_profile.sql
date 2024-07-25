60b61f25a49c1a0b119a7f318adeea1ea4d12f52
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-07-25 11:30:46 +0200

    refactor(supabase): format migration file

--- supabase/migrations/20240301133755_user_profile.sql
+++ supabase/migrations/20240301133755_user_profile.sql
@@ -1,47 +1,41 @@
-create table profiles (
-  id uuid not null references auth.users on delete cascade,
-  username text unique,
-  avatar_url text,
+CREATE TABLE
+  profiles (
+    id UUID NOT NULL REFERENCES auth.users ON DELETE CASCADE,
+    username TEXT UNIQUE,
+    avatar_url TEXT,
+    PRIMARY KEY (id),
+    CONSTRAINT username_length CHECK (CHAR_LENGTH(username) >= 3)
+  );
 
-  primary key (id),
-  constraint username_length check (char_length(username) >= 3)
-);
+ALTER TABLE profiles ENABLE ROW LEVEL SECURITY;
 
-alter table
-  profiles enable row level security;
+CREATE POLICY "profiles are visible for everyone" ON profiles FOR
+SELECT
+  USING (TRUE);
 
-create policy "profiles are visible for everyone"
-  on profiles
-    for select using (true);
+CREATE POLICY "profile can be inserted by owner user" ON profiles FOR INSERT
+WITH
+  CHECK (auth.uid () = id);
 
-create policy "profile can be inserted by owner user"
-  on profiles
-    for insert with check (auth.uid() = id);
+CREATE POLICY "profile can be updated by owner user" ON profiles FOR
+UPDATE USING (auth.uid () = id);
 
-create policy "profile can be updated by owner user"
-  on profiles
-    for update using (auth.uid() = id);
+CREATE FUNCTION public.handle_new_user () RETURNS TRIGGER AS $$
+DECLARE new_username text;
+BEGIN IF new.raw_user_meta_data->>'full_name' IS NOT NULL
+OR new.raw_user_meta_data->>'full_name' != '' THEN new_username = new.raw_user_meta_data->>'full_name';
+ELSE new_username = new.email;
+END IF;
+INSERT INTO public.profiles (id, username, avatar_url)
+VALUES (
+    new.id,
+    new_username,
+    new.raw_user_meta_data->>'avatar_url'
+  );
+RETURN new;
+END;
+$$ LANGUAGE plpgsql SECURITY DEFINER;
 
-create function public.handle_new_user() returns trigger as $$
-  declare
-    new_username text;
-  begin
-    if new.raw_user_meta_data->>'full_name' is not null or new.raw_user_meta_data->>'full_name' != ''
-      then new_username = new.raw_user_meta_data->>'full_name';
-    else
-      new_username = new.email;
-    end if;
-
-    insert into public.profiles (id, username, avatar_url)
-    values (
-        new.id,
-        new_username,
-        new.raw_user_meta_data->>'avatar_url'
-      );
-    return new;
-  end;
-  $$ language plpgsql security definer;
-
-create trigger on_auth_user_created
-  after insert on auth.users
-    for each row execute procedure public.handle_new_user();
+CREATE TRIGGER on_auth_user_created
+AFTER INSERT ON auth.users FOR EACH ROW
+EXECUTE PROCEDURE public.handle_new_user ();
