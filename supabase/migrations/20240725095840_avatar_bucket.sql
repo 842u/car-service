ec604965e504a4b57cd7e7b1a522b6f1288576c5
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-07-25 15:32:45 +0200

    chore(supabase): add avatar_bucket migration file

---
+++ supabase/migrations/20240725095840_avatar_bucket.sql
@@ -0,0 +1,50 @@
+INSERT INTO
+  STORAGE.buckets (id, NAME, public, file_size_limit, allowed_mime_types)
+VALUES
+  (
+    'avatars',
+    'avatars',
+    TRUE,
+    5242880,
+    ARRAY['image/jpeg', 'image/png', 'image/svg+xml', 'image/webp']
+  );
+
+ALTER TABLE STORAGE.buckets ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "public read acces to avatars bucket" ON STORAGE.objects FOR
+SELECT
+  TO authenticated,
+  anon USING (bucket_id = 'avatars');
+
+CREATE POLICY "only owner user can insert files into his folder" ON STORAGE.objects FOR INSERT TO authenticated
+WITH
+  CHECK (
+    bucket_id = 'avatars'
+    AND (
+      (STORAGE.foldername (NAME)) [1] = (
+        SELECT
+          auth.uid ()::TEXT
+      )
+    )
+  );
+
+CREATE POLICY "only owner user can update files in his folder" ON STORAGE.objects FOR
+UPDATE TO authenticated USING (
+  bucket_id = 'avatars'
+  AND (
+    (STORAGE.foldername (NAME)) [1] = (
+      SELECT
+        auth.uid ()::TEXT
+    )
+  )
+);
+
+CREATE POLICY "only owner user can delete files in his folder" ON STORAGE.objects FOR DELETE TO authenticated USING (
+  bucket_id = 'avatars'
+  AND (
+    (STORAGE.foldername (NAME)) [1] = (
+      SELECT
+        auth.uid ()::TEXT
+    )
+  )
+);
