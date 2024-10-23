2f78e2c5179a3336fd6db01df55754d4ba593d0e
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-10-23 17:47:04 +0200

    test(DashboardNavBar): add test file

---
+++ components/ui/NavBar/DashboardNavBar/DashboardNavBar.test.tsx
@@ -0,0 +1,65 @@
+import { render, screen } from '@testing-library/react';
+
+import { UserProfileProvider } from '@/components/providers/UserProfileProvider';
+
+import { USER_BADGE_TEST_ID } from '../../UserBadge/UserBadge';
+import { DashboardNavBar } from './DashboardNavBar';
+
+jest.mock('next/navigation', () => ({
+  useSelectedLayoutSegment: () => '/dashboard',
+  usePathname: () => '/dashboard',
+}));
+
+describe('DashboardNavBar', () => {
+  it('should render dashboard menu', () => {
+    render(
+      <UserProfileProvider>
+        <DashboardNavBar />
+      </UserProfileProvider>,
+    );
+
+    const dashboardMenu = screen.getByRole('navigation', {
+      name: /dashboard menu/i,
+    });
+
+    expect(dashboardMenu).toBeInTheDocument();
+  });
+
+  it('should render a brand logo link to landing page', () => {
+    render(
+      <UserProfileProvider>
+        <DashboardNavBar />
+      </UserProfileProvider>,
+    );
+
+    const landingPageLink = screen.getByRole('link', { name: /landing page/i });
+
+    expect(landingPageLink).toBeInTheDocument();
+  });
+
+  it('should render a hamburger button for nav menu toggle', () => {
+    render(
+      <UserProfileProvider>
+        <DashboardNavBar />
+      </UserProfileProvider>,
+    );
+
+    const menuToggleButton = screen.getByRole('button', {
+      name: /toggle navigation menu/i,
+    });
+
+    expect(menuToggleButton).toBeInTheDocument();
+  });
+
+  it('should render a user badge', () => {
+    render(
+      <UserProfileProvider>
+        <DashboardNavBar />
+      </UserProfileProvider>,
+    );
+
+    const userBadge = screen.getByTestId(USER_BADGE_TEST_ID);
+
+    expect(userBadge).toBeInTheDocument();
+  });
+});
