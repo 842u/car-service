ae5ddd7370f7b065a6cd8293c31b79bd4fcb19d7
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-10-25 13:56:12 +0200

    test(SubmitButton): add missing tests

--- components/ui/SubmitButton/SubmitButton.test.tsx
+++ components/ui/SubmitButton/SubmitButton.test.tsx
@@ -5,7 +5,6 @@ import { SubmitButton } from './SubmitButton';
 describe('SubmittButton', () => {
   it('should render a button', () => {
     const buttonText = 'some text';
-
     render(<SubmitButton>{buttonText}</SubmitButton>);
 
     const button = screen.getByRole('button', { name: buttonText });
@@ -15,11 +14,37 @@ describe('SubmittButton', () => {
 
   it('should render a loading spinner while is submitting', () => {
     const buttonText = 'some text';
-
     render(<SubmitButton isSubmitting>{buttonText}</SubmitButton>);
 
     const spinner = screen.getByTestId('spinner');
 
     expect(spinner).toBeInTheDocument();
+  });
+
+  it('should be enabled by default', () => {
+    const buttonText = 'some text';
+    render(<SubmitButton>{buttonText}</SubmitButton>);
+
+    const button = screen.getByRole('button', { name: buttonText });
+
+    expect(button).toBeEnabled();
+  });
+
+  it('should be disabled while disabled prop is true', () => {
+    const buttonText = 'some text';
+    render(<SubmitButton disabled>{buttonText}</SubmitButton>);
+
+    const button = screen.getByRole('button', { name: buttonText });
+
+    expect(button).toBeDisabled();
+  });
+
+  it('should be enabled while disabled prop is false', () => {
+    const buttonText = 'some text';
+    render(<SubmitButton disabled={false}>{buttonText}</SubmitButton>);
+
+    const button = screen.getByRole('button', { name: buttonText });
+
+    expect(button).toBeEnabled();
   });
 });
