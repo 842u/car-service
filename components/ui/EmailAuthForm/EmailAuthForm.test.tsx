6cd0801bd39d8585bd914af85a8c9c13bf15ac05
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-10-31 17:55:50 +0100

    test(EmailAuthForm): add missing tests

--- components/ui/EmailAuthForm/EmailAuthForm.test.tsx
+++ components/ui/EmailAuthForm/EmailAuthForm.test.tsx
@@ -1,5 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
+import { ToastsProvider } from '@/components/providers/ToastsProvider';
+
+import { SPINNER_TEST_ID } from '../Spinner/Spinner';
+import { Toaster } from '../Toaster/Toaster';
 import EmailAuthForm, { EmailAuthFormType } from './EmailAuthForm';
 
 jest.mock('next/navigation', () => ({
@@ -9,6 +14,12 @@ jest.mock('next/navigation', () => ({
 jest.mock('@supabase/ssr', () => ({
   createBrowserClient: () => ({}),
 }));
+
+const mockFetch = jest.fn();
+window.fetch = mockFetch;
+
+window.crypto.randomUUID = () =>
+  `${Date.now()}-${Math.random()}-some-random-uuid`;
 
 describe('EmailAuthForm', () => {
   it('should render email authentication form', () => {
@@ -55,5 +66,160 @@ describe('EmailAuthForm', () => {
 
       expect(text).toBeInTheDocument();
     });
+  });
+
+  it('should have submit button initially disabled', () => {
+    render(<EmailAuthForm type="sign-up" />);
+
+    const submitButton = screen.getByRole('button', { name: /sign up/i });
+
+    expect(submitButton).toBeDisabled();
+  });
+
+  it('should have submit button disabled if email or password are wrong', async () => {
+    const user = userEvent.setup();
+    const correctEmail = 'john.doe@example.com';
+    const correctPassword = 'password';
+    const wrongEmail = 'asd';
+    const wrongPassword = 'asd';
+    render(<EmailAuthForm type="sign-up" />);
+
+    const submitButton = screen.getByRole('button', { name: /sign up/i });
+    const emailInput = screen.getByRole('textbox', { name: /email/i });
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    await user.type(emailInput, correctEmail);
+    await user.type(passwordInput, wrongPassword);
+    await user.clear(emailInput);
+    await user.clear(passwordInput);
+
+    expect(submitButton).toBeDisabled();
+
+    await user.type(emailInput, wrongEmail);
+    await user.type(passwordInput, correctPassword);
+    await user.clear(emailInput);
+    await user.clear(passwordInput);
+
+    expect(submitButton).toBeDisabled();
+
+    await user.type(emailInput, wrongEmail);
+    await user.type(passwordInput, wrongPassword);
+    await user.clear(emailInput);
+    await user.clear(passwordInput);
+
+    expect(submitButton).toBeDisabled();
+  });
+
+  it('should have submit button enabled if email and password are correct', async () => {
+    const user = userEvent.setup();
+    const correctEmail = 'john.doe@example.com';
+    const correctPassword = 'password';
+    render(<EmailAuthForm type="sign-up" />);
+
+    const submitButton = screen.getByRole('button', { name: /sign up/i });
+    const emailInput = screen.getByRole('textbox', { name: /email/i });
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    await user.type(emailInput, correctEmail);
+    await user.type(passwordInput, correctPassword);
+
+    expect(submitButton).toBeEnabled();
+  });
+
+  it('should disable submit button and display loading spinner on submit', async () => {
+    const user = userEvent.setup();
+    const correctEmail = 'john.doe@example.com';
+    const correctPassword = 'password';
+    mockFetch.mockImplementationOnce(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(
+            () =>
+              resolve({
+                ok: true,
+                json: () => Promise.resolve({ message: 'data', error: null }),
+              }),
+            50,
+          );
+        }),
+    );
+    render(<EmailAuthForm type="sign-up" />);
+
+    const submitButton = screen.getByRole('button', { name: /sign up/i });
+    const emailInput = screen.getByRole('textbox', { name: /email/i });
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    await user.type(emailInput, correctEmail);
+    await user.type(passwordInput, correctPassword);
+    await user.click(submitButton);
+
+    const loadingSpinner = await screen.findByTestId(SPINNER_TEST_ID);
+
+    expect(submitButton).toBeDisabled();
+    expect(loadingSpinner).toBeInTheDocument();
+  });
+
+  it('should display success info on successful submit', async () => {
+    const user = userEvent.setup();
+    const correctEmail = 'john.doe@example.com';
+    const correctPassword = 'password';
+    mockFetch.mockResolvedValueOnce({
+      ok: true,
+      json: jest
+        .fn()
+        .mockResolvedValueOnce({ message: 'submit success', error: null }),
+    });
+    render(
+      <ToastsProvider>
+        <Toaster />
+        <EmailAuthForm type="sign-up" />
+      </ToastsProvider>,
+    );
+
+    const submitButton = screen.getByRole('button', { name: /sign up/i });
+    const emailInput = screen.getByRole('textbox', { name: /email/i });
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    await user.type(emailInput, correctEmail);
+    await user.type(passwordInput, correctPassword);
+    await user.click(submitButton);
+
+    const successToast = screen.getByRole('listitem', {
+      name: /success notification: submit success/i,
+    });
+
+    expect(successToast).toBeInTheDocument();
+  });
+
+  it('should display error info on unsuccessful submit', async () => {
+    const user = userEvent.setup();
+    const correctEmail = 'john.doe@example.com';
+    const correctPassword = 'password';
+    mockFetch.mockResolvedValueOnce({
+      ok: true,
+      json: jest
+        .fn()
+        .mockResolvedValueOnce({ message: null, error: 'submit error' }),
+    });
+    render(
+      <ToastsProvider>
+        <Toaster />
+        <EmailAuthForm type="sign-up" />
+      </ToastsProvider>,
+    );
+
+    const submitButton = screen.getByRole('button', { name: /sign up/i });
+    const emailInput = screen.getByRole('textbox', { name: /email/i });
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    await user.type(emailInput, correctEmail);
+    await user.type(passwordInput, correctPassword);
+    await user.click(submitButton);
+
+    const errorToast = screen.getByRole('listitem', {
+      name: /error notification: submit error/i,
+    });
+
+    expect(errorToast).toBeInTheDocument();
   });
 });
