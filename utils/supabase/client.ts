c6399dfe617c64863c5b65c6011a616974521e28
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-11-04 15:38:07 +0100

    refactor(utils/supabase): adjust client helpers functions

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -2,9 +2,15 @@ import { createBrowserClient } from '@supabase/ssr';
 
 import { Database } from '@/types/supabase';
 
-export function createClient() {
-  return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+const supabaseAppProtocol = process.env.APP_PROTOCOL!;
+const supabaseAppDomain = process.env.APP_DOMAIN!;
+const supabaseAppApiPort = process.env.APP_API_PORT!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseAppUrl = `${supabaseAppProtocol}://${supabaseAppDomain}:${supabaseAppApiPort}`;
+
+export function createClient(
+  supabaseUrl: string = supabaseAppUrl,
+  supabaseKey: string = supabaseAnonKey,
+) {
+  return createBrowserClient<Database>(supabaseUrl, supabaseKey);
 }
