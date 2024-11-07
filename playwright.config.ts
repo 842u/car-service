89dd7af2e9bcfd9257d6c53dbfe02eec65eeba75
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-11-07 12:34:51 +0100

    chore(playwright): adjust test setup for auth state

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
+import path from 'path';
+
+const authFile = path.join(__dirname, './playwright/.auth/user.json');
 
 export default defineConfig({
   testDir: './e2e',
@@ -11,24 +14,83 @@ export default defineConfig({
   use: { baseURL: 'http://localhost:3000', trace: 'on-first-retry' },
   projects: [
     {
+      name: 'Auth Setup',
+      testMatch: /.*\.setup\.ts/,
+      grep: /authenticated/,
+    },
+    {
       name: 'Desktop Chrome',
-      use: { ...devices['Desktop Chrome'] },
-      grep: /desktop/,
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: authFile,
+      },
+      grep: /@desktop/,
+      grepInvert: /@unauthenticated/,
+    },
+    {
+      name: 'Desktop Chrome',
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: undefined,
+      },
+      grep: /@desktop/,
+      grepInvert: /@authenticated/,
     },
     {
       name: 'Desktop Firefox',
-      use: { ...devices['Desktop Firefox'] },
-      grep: /desktop/,
+      use: {
+        ...devices['Desktop Firefox'],
+        storageState: authFile,
+      },
+      grep: /@desktop/,
+      grepInvert: /@unauthenticated/,
+    },
+    {
+      name: 'Desktop Firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+        storageState: undefined,
+      },
+      grep: /@desktop/,
+      grepInvert: /@authenticated/,
     },
     {
       name: 'Desktop Edge',
-      use: { ...devices['Desktop Edge'] },
-      grep: /desktop/,
+      use: {
+        ...devices['Desktop Edge'],
+        storageState: authFile,
+      },
+      grep: /@desktop/,
+      grepInvert: /@unauthenticated/,
+    },
+    {
+      name: 'Desktop Edge',
+      use: {
+        ...devices['Desktop Edge'],
+        storageState: undefined,
+      },
+      grep: /@desktop/,
+      grepInvert: /@authenticated/,
     },
     {
       name: 'Desktop Safari',
-      use: { ...devices['Desktop Safari'], deviceScaleFactor: 1 },
-      grep: /desktop/,
+      use: {
+        ...devices['Desktop Safari'],
+        deviceScaleFactor: 1,
+        storageState: authFile,
+      },
+      grep: /@desktop/,
+      grepInvert: /@unauthenticated/,
+    },
+    {
+      name: 'Desktop Safari',
+      use: {
+        ...devices['Desktop Safari'],
+        deviceScaleFactor: 1,
+        storageState: undefined,
+      },
+      grep: /@desktop/,
+      grepInvert: /@authenticated/,
     },
     {
       name: 'Tablet Chrome',
@@ -38,23 +100,77 @@ export default defineConfig({
           width: 834,
           height: 1194,
         },
+        storageState: authFile,
       },
-      grep: /tablet/,
+      grep: /@tablet/,
+      grepInvert: /@unauthenticated/,
+    },
+    {
+      name: 'Tablet Chrome',
+      use: {
+        ...devices['Galaxy Tab S4'],
+        viewport: {
+          width: 834,
+          height: 1194,
+        },
+        storageState: undefined,
+      },
+      grep: /@tablet/,
+      grepInvert: /@authenticated/,
     },
     {
       name: 'Tablet Safari',
-      use: { ...devices['iPad Pro 11'] },
-      grep: /tablet/,
+      use: {
+        ...devices['iPad Pro 11'],
+        storageState: authFile,
+      },
+      grep: /@tablet/,
+      grepInvert: /@unauthenticated/,
+    },
+    {
+      name: 'Tablet Safari',
+      use: {
+        ...devices['iPad Pro 11'],
+        storageState: undefined,
+      },
+      grep: /@tablet/,
+      grepInvert: /@authenticated/,
     },
     {
       name: 'Mobile Chrome',
-      use: { ...devices['Galaxy S9+'] },
-      grep: /mobile/,
+      use: {
+        ...devices['Galaxy S9+'],
+        storageState: authFile,
+      },
+      grep: /@mobile/,
+      grepInvert: /@unauthenticated/,
+    },
+    {
+      name: 'Mobile Chrome',
+      use: {
+        ...devices['Galaxy S9+'],
+        storageState: undefined,
+      },
+      grep: /@mobile/,
+      grepInvert: /@authenticated/,
     },
     {
       name: 'Mobile Safari',
-      use: { ...devices['iPhone 15 Pro Max'] },
-      grep: /mobile/,
+      use: {
+        ...devices['iPhone 15 Pro Max'],
+        storageState: authFile,
+      },
+      grep: /@mobile/,
+      grepInvert: /@unauthenticated/,
+    },
+    {
+      name: 'Mobile Safari',
+      use: {
+        ...devices['iPhone 15 Pro Max'],
+        storageState: undefined,
+      },
+      grep: /@mobile/,
+      grepInvert: /@authenticated/,
     },
   ],
   webServer: {
