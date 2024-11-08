9282c493eebe6041360a5f39878517d24f2a973d
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-11-08 15:15:53 +0100

    chore(playwright): increase test timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
-  timeout: 5000,
+  timeout: 7000,
   reporter: [['html', { open: 'never' }]],
   use: { baseURL: 'http://localhost:3000', trace: 'on-first-retry' },
   projects: [
