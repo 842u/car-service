c4ca21990ef4d7b03b499f8bc9eb550a1027da11
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-01-31 14:53:28 +0100

    feat(Button): add missing cursor indicators

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -11,7 +11,7 @@ export function Button({ className, children, ...props }: ButtonProps) {
       type="button"
       {...props}
       className={twMerge(
-        'border-alpha-grey-500 bg-dark-400 text-light-500 disabled:border-alpha-grey-300 disabled:bg-alpha-grey-200 disabled:text-alpha-grey-600 flex h-10 items-center justify-center rounded-md border px-4 py-2 transition-colors',
+        'border-alpha-grey-500 bg-dark-400 text-light-500 disabled:border-alpha-grey-300 disabled:bg-alpha-grey-200 disabled:text-alpha-grey-600 flex h-10 cursor-pointer items-center justify-center rounded-md border px-4 py-2 transition-colors disabled:cursor-not-allowed',
         className,
       )}
     >
