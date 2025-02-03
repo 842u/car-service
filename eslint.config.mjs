df5ba23bc6cab9aee27049fe15f2944bdbfa053a
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-02-03 13:34:47 +0100

    chore(eslint): add directories to ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,18 @@ import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    ignores: [
+      '.next/',
+      'coverage/',
+      'node_modules/',
+      'playwright/',
+      'playwright-report/',
+      'resources/',
+      'test-result/',
+    ],
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
