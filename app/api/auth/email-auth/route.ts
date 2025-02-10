3c8c41e3c8938b68a48faa50bf6312b633ba4fa5
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-02-10 15:21:59 +0100

    refactor(api/email-auth): fix typo

--- app/api/auth/email-auth/route.ts
+++ app/api/auth/email-auth/route.ts
@@ -85,7 +85,7 @@ export async function POST(request: NextRequest) {
 
       return NextResponse.json<RouteHandlerResponse>(
         {
-          message: 'Succesfully signed in.',
+          message: 'Successfully signed in.',
           error: null,
         },
         { status: 200 },
