d4922a8d328759309d2f1244522d4906521f8080
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-02-11 11:36:43 +0100

    feat(CarsPage): use AddCarButton

--- app/(dashboard)/dashboard/cars/page.tsx
+++ app/(dashboard)/dashboard/cars/page.tsx
@@ -1,7 +1,9 @@
+import { AddCarButton } from '@/components/ui/AddCarButton/AddCarButton';
+
 export default function CarsPage() {
   return (
     <main className="flex h-screen flex-col items-center justify-center">
-      <h1>CARS</h1>
+      <AddCarButton className="fixed right-0 bottom-0 m-4 md:m-8 lg:m-12" />
     </main>
   );
 }
