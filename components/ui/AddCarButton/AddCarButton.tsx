02a345c8a3f3bb6e7567bb0a58572e2273acdc3d
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-02-11 11:32:17 +0100

    feat(AddCarButton): add component props

--- components/ui/AddCarButton/AddCarButton.tsx
+++ components/ui/AddCarButton/AddCarButton.tsx
@@ -1,9 +1,21 @@
+import { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
+
 import { CarPlusIcon } from '@/components/decorative/icons/CarPlusIcon';
 
-export function AddCarButton() {
+type AddCarButtonProps = ComponentProps<'button'> & {
+  className?: string;
+};
+
+export function AddCarButton({ className }: AddCarButtonProps) {
   return (
-    <button className="border-accent-400 bg-accent-800 hover:bg-accent-700 dark:bg-accent-900 dark:hover:bg-accent-800 aspect-square w-36 cursor-pointer rounded-2xl border-2 transition-colors">
-      <CarPlusIcon className="m-4 stroke-2" />
+    <button
+      className={twMerge(
+        'border-accent-400 bg-accent-800 hover:bg-accent-700 dark:bg-accent-900 dark:hover:bg-accent-800 aspect-square w-20 cursor-pointer rounded-2xl border-1 transition-colors',
+        className,
+      )}
+    >
+      <CarPlusIcon className="m-2 stroke-2" />
     </button>
   );
 }
