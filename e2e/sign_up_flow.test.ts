f09b66907448108676a16a825b67fe1226762870
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-02-18 16:16:38 +0100

    test(sign_up_flow): refactor for better performance

--- e2e/sign_up_flow.test.ts
+++ e2e/sign_up_flow.test.ts
@@ -1,38 +1,36 @@
-import test, { expect } from '@playwright/test';
 import { Route } from 'next';
 
 import { createTestUser, deleteTestUser } from '@/utils/supabase/general';
+import { wrongEmails } from '@/utils/validation';
+
+import { expect, test } from './fixtures';
 
 test.describe('sign_up_flow - @unauthenticated', () => {
-  // For some reason, the webkit browser requires input to be set to a textbox instead of a password. It may not be able to read the value of the password type.
-
   test('submit button should be disabled if sign up email auth form is filled incorrectly - @desktop @tablet @mobile', async ({
     page,
   }) => {
     const signUpPage: Route = '/dashboard/sign-up';
-    const wrongFormatEmail = 'wrong@email';
+    const wrongFormatEmail = wrongEmails[0];
     const wrongFormatPassword = 'wrong';
 
     await page.goto(signUpPage);
     const emailInput = page.getByPlaceholder(/enter your email/i);
     const passwordInput = page.getByPlaceholder(/enter your password/i);
     const submitButton = page.getByLabel('Sign Up', { exact: true });
-
     const togglePasswordVisibility = page.getByLabel(/toggle visibility/i);
     await togglePasswordVisibility.click();
     await emailInput.fill(wrongFormatEmail);
     await passwordInput.fill(wrongFormatPassword);
 
     await expect(submitButton).toBeDisabled();
-    await expect(page).toHaveURL(signUpPage);
   });
 
-  test('on successful sign up success info should be displayed - @desktop @tablet @mobile', async ({
+  test('success info should be displayed on successful sign up - @desktop @tablet @mobile', async ({
     page,
   }) => {
+    const workerIndex = test.info().workerIndex;
     const signUpPage: Route = '/dashboard/sign-up';
-    const testUserIndex = test.info().workerIndex;
-    const testUserEmail = testUserIndex + process.env.SUPABASE_TEST_USER_EMAIL!;
+    const testUserEmail = workerIndex + process.env.SUPABASE_TEST_USER_EMAIL!;
     const testUserPassword = process.env.SUPABASE_TEST_USER_PASSWORD!;
 
     await page.goto(signUpPage);
@@ -49,17 +47,15 @@ test.describe('sign_up_flow - @unauthenticated', () => {
     await expect(successToast).toBeInViewport();
     await expect(page).toHaveURL(signUpPage);
 
-    await deleteTestUser(testUserIndex);
+    await deleteTestUser(workerIndex);
   });
 
-  test('on existing user email sign up success info should be displayed - @desktop @tablet @mobile', async ({
+  test('success info should be displayed on existing user email sign up - @desktop @tablet @mobile', async ({
     page,
   }) => {
-    const testUserIndex = test.info().workerIndex;
-    await createTestUser(testUserIndex);
+    const workerIndex = test.info().workerIndex;
+    const { email, password } = await createTestUser(workerIndex);
     const signUpPage: Route = '/dashboard/sign-up';
-    const testUserEmail = testUserIndex + process.env.SUPABASE_TEST_USER_EMAIL!;
-    const testUserPassword = process.env.SUPABASE_TEST_USER_PASSWORD!;
 
     await page.goto(signUpPage);
     const emailInput = page.getByPlaceholder(/enter your email/i);
@@ -67,14 +63,14 @@ test.describe('sign_up_flow - @unauthenticated', () => {
     const submitButton = page.getByLabel('Sign Up', { exact: true });
     const togglePasswordVisibility = page.getByLabel(/toggle visibility/i);
     await togglePasswordVisibility.click();
-    await emailInput.fill(testUserEmail);
-    await passwordInput.fill(testUserPassword);
+    await emailInput.fill(email);
+    await passwordInput.fill(password);
     await submitButton.click();
     const successToast = page.getByLabel(/success notification/i);
 
     await expect(successToast).toBeInViewport();
     await expect(page).toHaveURL(signUpPage);
 
-    await deleteTestUser(testUserIndex);
+    await deleteTestUser(workerIndex);
   });
 });
