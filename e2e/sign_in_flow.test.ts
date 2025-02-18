34442e81a19fbbee1537654c1972c66e7eb05fbc
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-02-18 16:06:56 +0100

    test(sign_in_flow): refactor for better performance

--- e2e/sign_in_flow.test.ts
+++ e2e/sign_in_flow.test.ts
@@ -1,66 +1,16 @@
-import { expect, test } from '@playwright/test';
 import { Route } from 'next';
 
 import { createTestUser, deleteTestUser } from '@/utils/supabase/general';
+import { wrongEmails } from '@/utils/validation';
+
+import { expect, test } from './fixtures';
 
 test.describe('sign_in_flow - @unauthenticated', () => {
-  test('go to sign in page from landing page - @desktop @tablet @mobile', async ({
-    page,
-  }) => {
-    const homePagePath: Route = '/';
-    const signInPath: Route = '/dashboard/sign-in';
-
-    await page.goto(homePagePath);
-    const signInLink = page.getByRole('link', { name: /sign in/i });
-    await signInLink.click();
-
-    await expect(page).toHaveURL(signInPath);
-  });
-
-  test('go to sign in page from navigation - @desktop', async ({ page }) => {
-    const homePagePath: Route = '/';
-    const signInPath: Route = '/dashboard/sign-in';
-
-    await page.goto(homePagePath);
-    const dashboardLink = page.getByRole('link', { name: /dashboard/i });
-    await dashboardLink.click();
-
-    await expect(page).toHaveURL(signInPath);
-  });
-
-  test('go to sign in page from navigation - @tablet @mobile', async ({
-    page,
-  }) => {
-    const homePagePath: Route = '/';
-    const signInPath: Route = '/dashboard/sign-in';
-
-    await page.goto(homePagePath);
-    const hamburgerButton = page.getByLabel(/toggle navigation menu/i);
-    await hamburgerButton.click();
-    const dashboardLink = page.getByRole('link', { name: /dashboard/i });
-    await dashboardLink.click();
-
-    await expect(page).toHaveURL(signInPath);
-  });
-
-  test('go to sign in page from sign up page - @desktop @tablet @mobile', async ({
-    page,
-  }) => {
-    const signUpPath: Route = '/dashboard/sign-up';
-    const signInPath: Route = '/dashboard/sign-in';
-
-    await page.goto(signUpPath);
-    const signInLink = page.getByRole('link', { name: /sign in/i });
-    await signInLink.click();
-
-    await expect(page).toHaveURL(signInPath);
-  });
-
-  test('submit button should be disabled if sign in email auth form is filled incorrectly - @desktop @tablet @mobile', async ({
+  test('submit button should be disabled if auth form is filled incorrectly - @desktop @tablet @mobile', async ({
     page,
   }) => {
     const signInPath: Route = '/dashboard/sign-in';
-    const wrongEmail = 'wrong@email';
+    const wrongEmail = wrongEmails[0];
     const wrongPassword = 'wrong';
 
     await page.goto(signInPath);
@@ -71,25 +21,23 @@ test.describe('sign_in_flow - @unauthenticated', () => {
     const submitButton = page.getByRole('button', { name: /sign in/i });
 
     await expect(submitButton).toBeDisabled();
-    await expect(page).toHaveURL(signInPath);
   });
 
-  test('should stay on sign in page if wrong credentials provided and error info should be displayed - @desktop @tablet @mobile', async ({
+  test('error info should be displayed if wrong credentials provided - @desktop @tablet @mobile', async ({
     page,
   }) => {
-    await deleteTestUser();
-    const testUserEmail = process.env.SUPABASE_TEST_USER_EMAIL!;
-    const testUserPassword = process.env.SUPABASE_TEST_USER_PASSWORD!;
+    const nonExistingEmail = `${crypto.randomUUID()}@${crypto.randomUUID()}.com`;
+    const password = 'someCorrectPassword';
     const signInPath: Route = '/dashboard/sign-in';
 
     await page.goto(signInPath);
     const emailInput = page.getByPlaceholder(/enter your email/i);
     const passwordInput = page.getByPlaceholder(/enter your password/i);
-    // For some reason, the webkit browser requires input to be set to a textbox instead of a password. It may not be able to read the value of the password type.
+
     const togglePasswordVisibility = page.getByLabel(/toggle visibility/i);
     await togglePasswordVisibility.click();
-    await emailInput.fill(testUserEmail);
-    await passwordInput.fill(testUserPassword);
+    await emailInput.fill(nonExistingEmail);
+    await passwordInput.fill(password);
     const submitButton = page.getByRole('button', { name: /sign in/i });
     await submitButton.click();
     const errorToast = page.getByLabel(/error notification/i);
@@ -99,30 +47,28 @@ test.describe('sign_in_flow - @unauthenticated', () => {
     await expect(page).toHaveURL(signInPath);
   });
 
-  test('should go to dashboard page on successful email auth sign in and success info should be displayed - @desktop @tablet @mobile', async ({
+  test('success info should be displayed on successful auth form sign in - @desktop @tablet @mobile', async ({
     page,
   }) => {
-    await deleteTestUser();
-    await createTestUser();
-    const testUserEmail = process.env.SUPABASE_TEST_USER_EMAIL!;
-    const testUserPassword = process.env.SUPABASE_TEST_USER_PASSWORD!;
+    const workerIndex = test.info().workerIndex;
+    const { email, password } = await createTestUser(workerIndex);
     const signInPath: Route = '/dashboard/sign-in';
     const dashboardPath: Route = '/dashboard';
 
     await page.goto(signInPath);
     const emailInput = page.getByPlaceholder(/enter your email/i);
     const passwordInput = page.getByPlaceholder(/enter your password/i);
-    // For some reason, the webkit browser requires input to be set to a textbox instead of a password. It may not be able to read the value of the password type.
     const togglePasswordVisibility = page.getByLabel(/toggle visibility/i);
     await togglePasswordVisibility.click();
-    await emailInput.fill(testUserEmail);
-    await passwordInput.fill(testUserPassword);
+    await emailInput.fill(email);
+    await passwordInput.fill(password);
     const submitButton = page.getByRole('button', { name: /sign in/i });
     await submitButton.click();
     const successToast = page.getByLabel(/success notification/i);
 
     await expect(page).toHaveURL(dashboardPath);
     await expect(successToast).toBeInViewport();
-    await deleteTestUser();
+
+    await deleteTestUser(workerIndex);
   });
 });
