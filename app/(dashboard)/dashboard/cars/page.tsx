c0cf3b778bea14bdee1902ae5bd4f5d45580d016
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-02-26 17:15:07 +0100

    feat(CarsPage): fetch cars data

--- app/(dashboard)/dashboard/cars/page.tsx
+++ app/(dashboard)/dashboard/cars/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
 import { useRef } from 'react';
 
 import { AddCarButton } from '@/components/ui/AddCarButton/AddCarButton';
@@ -8,12 +9,26 @@ import {
   DialogModal,
   DialogModalRef,
 } from '@/components/ui/DialogModal/DialogModal';
+import { createClient } from '@/utils/supabase/client';
+
+const fetchCars = async () => {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.from('cars').select();
+
+  if (error) throw new Error(error.message);
+
+  return data;
+};
 
 export default function CarsPage() {
   const addCarModalRef = useRef<DialogModalRef>(null);
 
+  const { data } = useQuery({ queryKey: ['cars'], queryFn: fetchCars });
+
   return (
     <main className="flex h-screen flex-col items-center justify-center">
+      {data?.map((car) => <p key={car.id}>{car.id}</p>)}
       <DialogModal ref={addCarModalRef}>
         <AddCarForm />
       </DialogModal>
