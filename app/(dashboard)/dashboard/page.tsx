1e213f3bc612bdd2cd2bf0982faa3adf475cc816
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-02-26 17:15:57 +0100

    refactor(DashboardPage): cleanup

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,16 +1,7 @@
-import { createClient } from '@/utils/supabase/server';
-
 export default async function DashboardPage() {
-  const supabase = await createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
   return (
     <main className="flex h-screen flex-col items-center justify-center">
       <h1>DASHBOARD</h1>
-      <p>{user?.email}</p>
     </main>
   );
 }
