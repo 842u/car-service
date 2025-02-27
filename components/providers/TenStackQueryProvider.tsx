b187be4c76724ddcc294c430ffb2a430f72fe218
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-02-27 09:55:37 +0100

    refactor(TenStackQueryProvider): store QueryClient in state

--- components/providers/TenStackQueryProvider.tsx
+++ components/providers/TenStackQueryProvider.tsx
@@ -2,17 +2,17 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 
 type TenStackQueryProviderProps = {
   children: ReactNode;
 };
 
-const queryClient = new QueryClient();
-
 export function TenStackQueryProvider({
   children,
 }: TenStackQueryProviderProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
