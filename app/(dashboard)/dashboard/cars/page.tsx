9808ff2a1bb86aa27cf9950d4c77401930f2782f
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-03-04 11:12:35 +0100

    refactor(CarsPage): use useInfiniteQuery for cars

--- app/(dashboard)/dashboard/cars/page.tsx
+++ app/(dashboard)/dashboard/cars/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useQuery } from '@tanstack/react-query';
-import { useRef } from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useEffect, useRef } from 'react';
 
 import { AddCarButton } from '@/components/ui/AddCarButton/AddCarButton';
 import { AddCarForm } from '@/components/ui/AddCarForm/AddCarForm';
@@ -12,28 +12,69 @@ import {
 } from '@/components/ui/DialogModal/DialogModal';
 import { createClient } from '@/utils/supabase/client';
 
-const fetchCars = async () => {
+const fetchCars = async ({ pageParam }: { pageParam: number }) => {
+  const rangeIndexFrom = pageParam * 15;
+  const rangeIndexTo = (pageParam + 1) * 15 - 1;
   const supabase = createClient();
 
   const { data, error } = await supabase
     .from('cars')
     .select()
-    .order('created_at', { ascending: false });
+    .order('created_at', { ascending: false })
+    .limit(15)
+    .range(rangeIndexFrom, rangeIndexTo);
 
   if (error) throw new Error(error.message);
 
-  return data;
+  return { data, nextPageParam: pageParam + 1 };
 };
 
 export default function CarsPage() {
   const addCarModalRef = useRef<DialogModalRef>(null);
+  const bottomRef = useRef<HTMLDivElement>(null);
 
-  const { data } = useQuery({ queryKey: ['cars'], queryFn: fetchCars });
+  const {
+    data,
+    error,
+    status,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: ['cars'],
+    queryFn: fetchCars,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, _page) => lastPage.nextPageParam,
+  });
 
-  return (
+  useEffect(() => {
+    if (!bottomRef.current || !hasNextPage) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          !isFetchingNextPage && fetchNextPage();
+        }
+      },
+      { threshold: 1.0, rootMargin: '200px' },
+    );
+
+    observer.observe(bottomRef.current);
+
+    return () => observer.disconnect();
+  }, [fetchNextPage, hasNextPage, isFetchingNextPage]);
+
+  return status === 'pending' ? (
+    <p>Loading...</p>
+  ) : status === 'error' ? (
+    <p>Error: {error.message}</p>
+  ) : (
     <main className="flex h-screen flex-col items-center justify-center">
-      <div className="relative top-16 flex h-full max-w-[1920px] flex-col items-center justify-around gap-5 py-5 md:flex-row md:flex-wrap md:justify-center md:gap-10 md:pl-16">
-        {data?.map((car) => <CarCard key={car.id} car={car} />)}
+      <div className="relative top-16 flex h-full w-full max-w-[1920px] flex-col items-center justify-around gap-5 py-5 md:flex-row md:flex-wrap md:justify-center md:gap-10 md:pl-16">
+        {data.pages.map((page) => {
+          return page.data.map((car) => <CarCard key={car.id} car={car} />);
+        })}
+        <div ref={bottomRef} className="w-full" />
       </div>
       <DialogModal ref={addCarModalRef}>
         <AddCarForm onSubmit={() => addCarModalRef.current?.closeModal()} />
