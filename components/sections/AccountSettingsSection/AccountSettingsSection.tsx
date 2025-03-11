2af99a31ee4ee34f6573cb201c67c288e394f946
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-03-11 09:24:54 +0100

    refactor(UsernameSection): rename to UsernameForm

--- components/sections/AccountSettingsSection/AccountSettingsSection.tsx
+++ components/sections/AccountSettingsSection/AccountSettingsSection.tsx
@@ -3,13 +3,18 @@
 import { PasswordResetForm } from '@/components/ui/PasswordResetForm/PasswordResetForm';
 import { SettingsSection } from '@/components/ui/SettingsSection/SettingsSection';
 
+import { UsernameForm } from '../../ui/UsernameForm/UsernameForm';
 import { AvatarSection } from '../AvatarSection/AvatarSection';
-import { UsernameSection } from '../UsernameSection/UsernameSection';
 
 export function AccountSettingsSection() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4">
-      <UsernameSection />
+    <section
+      aria-label="account settings"
+      className="flex flex-col items-center justify-center gap-4"
+    >
+      <SettingsSection headingText="Username">
+        <UsernameForm />
+      </SettingsSection>
       <AvatarSection />
       <SettingsSection headingText="Reset password">
         <PasswordResetForm />
--- components/ui/UsernameForm/UsernameForm.tsx
+++ components/ui/UsernameForm/UsernameForm.tsx
@@ -1,10 +1,11 @@
+'use client';
+
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 
 import { Button } from '@/components/ui/Button/Button';
 import { Input } from '@/components/ui/Input/Input';
-import { SettingsSection } from '@/components/ui/SettingsSection/SettingsSection';
 import { SubmitButton } from '@/components/ui/SubmitButton/SubmitButton';
 import { useToasts } from '@/hooks/useToasts';
 import { getProfile, patchProfile } from '@/utils/supabase/general';
@@ -18,7 +19,7 @@ export type UsernameFormValues = {
   username: string;
 };
 
-export function UsernameSection() {
+export function UsernameForm() {
   const { addToast } = useToasts();
 
   const { data, error, isSuccess, isError } = useQuery({
@@ -70,51 +71,49 @@ export function UsernameSection() {
   }, [isError, addToast, error]);
 
   return (
-    <SettingsSection headingText="Username">
-      <form
-        className="items-center justify-between lg:flex"
-        onSubmit={handleSubmit(submitHandler)}
-      >
-        <div className="lg:w-1/3 lg:p-4">
-          <Input
-            errorMessage={errors.username?.message}
-            label="Username"
-            name="username"
-            placeholder="Enter your username"
-            register={register}
-            registerOptions={usernameValidationRules}
-            showErrorMessage={false}
-            type="text"
-          />
-        </div>
-        <div className="w-full flex-1">
-          <div className="text-sm">
-            <p>
-              Please enter your full name, or a display name you are comfortable
-              with.
-            </p>
-            <p className="text-alpha-grey-700">
-              {`Letters, numbers and single whitespaces allowed. Length between
+    <form
+      className="items-center justify-between lg:flex"
+      onSubmit={handleSubmit(submitHandler)}
+    >
+      <div className="lg:w-1/3 lg:p-4">
+        <Input
+          errorMessage={errors.username?.message}
+          label="Username"
+          name="username"
+          placeholder="Enter your username"
+          register={register}
+          registerOptions={usernameValidationRules}
+          showErrorMessage={false}
+          type="text"
+        />
+      </div>
+      <div className="w-full flex-1">
+        <div className="text-sm">
+          <p>
+            Please enter your full name, or a display name you are comfortable
+            with.
+          </p>
+          <p className="text-alpha-grey-700">
+            {`Letters, numbers and single whitespaces allowed. Length between
               ${usernameValidationRules.minLength.value} and
               ${usernameValidationRules.maxLength.value} characters.`}
-            </p>
-          </div>
-          <div className="my-4 flex justify-center gap-4">
-            <SubmitButton className="flex-1" disabled={!isValid || !isDirty}>
-              Save
-            </SubmitButton>
-            <Button
-              className="flex-1"
-              disabled={!isDirty}
-              onClick={() => {
-                reset({ username: data?.username || '' });
-              }}
-            >
-              Cancel
-            </Button>
-          </div>
+          </p>
         </div>
-      </form>
-    </SettingsSection>
+        <div className="my-4 flex justify-center gap-4">
+          <SubmitButton className="flex-1" disabled={!isValid || !isDirty}>
+            Save
+          </SubmitButton>
+          <Button
+            className="flex-1"
+            disabled={!isDirty}
+            onClick={() => {
+              reset({ username: data?.username || '' });
+            }}
+          >
+            Cancel
+          </Button>
+        </div>
+      </div>
+    </form>
   );
 }
