df5607ce7616c224325ea9adc8f4b2eb1add60db
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-03-13 12:35:56 +0100

    refactor(NavBar): cleanup structure and style

--- components/ui/NavBar/NavBar.tsx
+++ components/ui/NavBar/NavBar.tsx
@@ -6,11 +6,7 @@ type NavBarProps = {
 
 export function NavBar({ children }: NavBarProps) {
   return (
-    <header className="border-alpha-grey-300 fixed z-50 flex h-16 w-full justify-between border-b px-2 md:px-4 lg:px-6">
-      <div
-        aria-hidden
-        className="border-alpha-grey-300 bg-light-500 dark:bg-dark-500 absolute left-0 z-0 box-content h-full w-full border-b transition-all"
-      />
+    <header className="bg-light-500 dark:bg-dark-500 border-alpha-grey-300 sticky top-0 z-10 flex h-16 w-full justify-between border-b px-2 md:px-4 lg:px-6">
       {children}
     </header>
   );
