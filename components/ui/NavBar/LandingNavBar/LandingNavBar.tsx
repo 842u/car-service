bc75952f154704b1c41f6cf87fb950d656061b1b
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-03-13 13:52:49 +0100

    style(LandingNavMenu): cleanup

--- components/ui/NavBar/LandingNavBar/LandingNavBar.tsx
+++ components/ui/NavBar/LandingNavBar/LandingNavBar.tsx
@@ -20,15 +20,19 @@ export function LandingNavBar() {
 
   return (
     <NavBar>
-      <BrandLabel />
+      <div
+        aria-hidden
+        className="border-alpha-grey-300 bg-light-500 dark:bg-dark-500 absolute top-0 left-0 z-10 h-full w-full border-b"
+      />
+      <BrandLabel className="z-20" />
       <HamburgerButton
         aria-label="toggle navigation menu"
-        className="lg:hidden"
+        className="z-20 lg:hidden"
         isActive={isActive}
         onClick={hamburgerButtonClickHandler}
       />
       <LandingNavMenu
-        className="z-0"
+        className="z-0 lg:z-20"
         isActive={isActive}
         onClick={navMenuClickHandler}
       />
--- components/ui/NavBar/NavBar.tsx
+++ components/ui/NavBar/NavBar.tsx
@@ -6,7 +6,7 @@ type NavBarProps = {
 
 export function NavBar({ children }: NavBarProps) {
   return (
-    <header className="bg-light-500 dark:bg-dark-500 border-alpha-grey-300 sticky top-0 z-10 flex h-16 w-full justify-between border-b px-2 md:px-4 lg:px-6">
+    <header className="sticky top-0 z-10 flex h-16 w-full justify-between px-2 md:px-4 lg:px-6">
       {children}
     </header>
   );
