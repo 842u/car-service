532010d9dbbb19cda3006614b3c5ef2dacb8eaae
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-03-13 15:50:16 +0100

    style(CTASection): adjust

--- components/sections/CTASection/CTASection.tsx
+++ components/sections/CTASection/CTASection.tsx
@@ -4,8 +4,8 @@ import { Section } from '../Section';
 
 export function CTASection() {
   return (
-    <Section className="relative flex h-[25vh] flex-col items-center justify-center text-center">
-      <h2 className="my-10 text-xl font-medium md:text-2xl">
+    <Section className="flex h-[50vh] flex-col items-center justify-center gap-10 text-center">
+      <h2 className="text-xl font-medium md:text-2xl">
         <p className="text-alpha-grey-900 md:mr-2 md:inline-block">
           Car&apos;s Story Safely Managed.
         </p>
