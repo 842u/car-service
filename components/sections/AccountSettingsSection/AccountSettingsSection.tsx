6181b4d0019611eeee5d713528c3d35ab90a5a44
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-03-14 15:03:51 +0100

    feat(AccountSettingsSection): use useQuery for profile data

--- components/sections/AccountSettingsSection/AccountSettingsSection.tsx
+++ components/sections/AccountSettingsSection/AccountSettingsSection.tsx
@@ -1,20 +1,38 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+
 import { AvatarForm } from '@/components/ui/AvatarForm/AvatarForm';
 import { PasswordResetForm } from '@/components/ui/PasswordResetForm/PasswordResetForm';
 import { SettingsSection } from '@/components/ui/SettingsSection/SettingsSection';
+import { useToasts } from '@/hooks/useToasts';
+import { getProfile } from '@/utils/supabase/general';
 
 import { UsernameForm } from '../../ui/UsernameForm/UsernameForm';
 
 export function AccountSettingsSection() {
+  const { addToast } = useToasts();
+
+  const { data, error, isError } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  });
+
+  useEffect(() => {
+    isError && addToast(error.message, 'error');
+  }, [isError, addToast, error]);
+
   return (
     <section
       aria-label="account settings"
       className="flex flex-col items-center justify-center gap-4 p-5"
     >
       <SettingsSection headingText="Username">
-        <UsernameForm />
+        <UsernameForm data={data} />
       </SettingsSection>
       <SettingsSection headingText="Avatar">
-        <AvatarForm />
+        <AvatarForm data={data} />
       </SettingsSection>
       <SettingsSection headingText="Reset password">
         <PasswordResetForm />
