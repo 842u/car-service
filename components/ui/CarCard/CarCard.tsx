52164e4c4ffe9dca3d73e88e13940f89fc7cc880
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-03-14 16:54:56 +0100

    refactor(CarCard): wrap in Link

--- components/ui/CarCard/CarCard.tsx
+++ components/ui/CarCard/CarCard.tsx
@@ -1,3 +1,6 @@
+import { Route } from 'next';
+import Link from 'next/link';
+
 import { Car } from '@/types';
 
 import { Card } from '../Card/Card';
@@ -8,26 +11,33 @@ type CarCardProps = {
 };
 
 export function CarCard({ car }: CarCardProps) {
+  const staticSegment = '/dashboard/cars' satisfies Route;
+
   return (
-    <Card className="flex w-80 flex-col justify-between self-stretch">
-      <CarImage className="relative aspect-square w-full" src={car.image_url} />
-      <div>
-        <p className="my-4 text-3xl font-semibold break-words">
-          {car.custom_name}
-        </p>
-        <p className="my-2 whitespace-pre-wrap">
-          <span>Brand: </span>
-          <span className="text-accent-400">{car.brand || ''}</span>
-        </p>
-        <p className="my-2 whitespace-pre-wrap">
-          <span>Model: </span>
-          <span className="text-accent-400">{car.model || ''}</span>
-        </p>
-        <p className="my-2 whitespace-pre-wrap">
-          <span>License Plates: </span>
-          <span className="text-accent-400">{car.license_plates || ''}</span>
-        </p>
-      </div>
-    </Card>
+    <Link href={`${staticSegment}/${car.id}`}>
+      <Card className="flex w-80 flex-col justify-between self-stretch">
+        <CarImage
+          className="relative aspect-square w-full"
+          src={car.image_url}
+        />
+        <div>
+          <p className="my-4 text-3xl font-semibold break-words">
+            {car.custom_name}
+          </p>
+          <p className="my-2 whitespace-pre-wrap">
+            <span>Brand: </span>
+            <span className="text-accent-400">{car.brand || ''}</span>
+          </p>
+          <p className="my-2 whitespace-pre-wrap">
+            <span>Model: </span>
+            <span className="text-accent-400">{car.model || ''}</span>
+          </p>
+          <p className="my-2 whitespace-pre-wrap">
+            <span>License Plates: </span>
+            <span className="text-accent-400">{car.license_plates || ''}</span>
+          </p>
+        </div>
+      </Card>
+    </Link>
   );
 }
