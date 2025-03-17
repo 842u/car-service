bba76893be0165499caed428114aafe36dc38d53
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-03-17 17:20:42 +0100

    feat(AccountSettingsSection): add IdSection

--- components/sections/AccountSettingsSection/AccountSettingsSection.tsx
+++ components/sections/AccountSettingsSection/AccountSettingsSection.tsx
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import { useEffect } from 'react';
 
 import { AvatarForm } from '@/components/ui/AvatarForm/AvatarForm';
+import { IdSection } from '@/components/ui/IdSection/IdSection';
 import { PasswordResetForm } from '@/components/ui/PasswordResetForm/PasswordResetForm';
 import { SettingsSection } from '@/components/ui/SettingsSection/SettingsSection';
 import { useToasts } from '@/hooks/useToasts';
@@ -28,6 +29,7 @@ export function AccountSettingsSection() {
       aria-label="account settings"
       className="flex flex-col items-center justify-center gap-4 p-5"
     >
+      <IdSection id={data?.id} />
       <SettingsSection headingText="Username">
         <UsernameForm data={data} />
       </SettingsSection>
