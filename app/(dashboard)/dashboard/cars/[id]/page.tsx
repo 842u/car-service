a74c840f2358c539daa61c546d7e378b6f88a97c
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-03-17 15:37:12 +0100

    feat(CarPage): add CarDetailsSection

--- app/(dashboard)/dashboard/cars/[id]/page.tsx
+++ app/(dashboard)/dashboard/cars/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CarDetailsSection from '@/components/ui/CarDetailsSection/CarDetailsSection';
 import { DashboardMain } from '@/components/ui/DashboardMain/DashboardMain';
 
 type CarPageProps = {
@@ -7,5 +8,9 @@ type CarPageProps = {
 export default async function CarPage({ params }: CarPageProps) {
   const { id } = await params;
 
-  return <DashboardMain>{id}</DashboardMain>;
+  return (
+    <DashboardMain className="px-5 pt-21">
+      <CarDetailsSection id={id} />
+    </DashboardMain>
+  );
 }
