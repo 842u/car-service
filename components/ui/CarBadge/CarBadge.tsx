72399f4ad2c29795e3de4b86e189c4a0ec7802ea
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-03-18 10:28:29 +0100

    feat(CarBadge): create component

---
+++ components/ui/CarBadge/CarBadge.tsx
@@ -0,0 +1,24 @@
+import { CarImage } from '../CarImage/CarImage';
+import { Spinner } from '../Spinner/Spinner';
+
+type CarBadgeProps = {
+  name?: string;
+  imageUrl?: string | null;
+  isPending?: boolean;
+};
+
+export function CarBadge({ imageUrl, name, isPending }: CarBadgeProps) {
+  return (
+    <div className="border-alpha-grey-300 flex flex-col-reverse items-center gap-5 rounded-lg border p-2 md:flex-row">
+      <div className="w-full max-w-md overflow-hidden rounded-lg md:max-w-xs md:basis-1/4">
+        {isPending && (
+          <Spinner className="stroke-accent-400 fill-accent-400 h-full w-full p-20 md:p-10" />
+        )}
+        {!isPending && <CarImage src={imageUrl} />}
+      </div>
+      <h1 className="grow text-center text-3xl break-all whitespace-pre-wrap lg:break-normal">
+        {name || ' '}
+      </h1>
+    </div>
+  );
+}
