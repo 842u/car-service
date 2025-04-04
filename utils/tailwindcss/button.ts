ecf9863e2e0bd40c3d47bbdbd592769ca48039c6
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-04 10:56:31 +0200

    style: adjust button accent variant

--- utils/tailwindcss/button.ts
+++ utils/tailwindcss/button.ts
@@ -4,7 +4,7 @@ const defaultClassName =
   'border-alpha-grey-500 bg-alpha-grey-200 disabled:bg-alpha-grey-50 disabled:border-alpha-grey-100 hover:border-alpha-grey-600 hover:bg-alpha-grey-500 cursor-pointer rounded-lg border disabled:cursor-not-allowed';
 
 const accentClassName =
-  'border-accent-400 bg-accent-700 disabled:bg-accent-800 disabled:border-accent-600 hover:border-accent-200 hover:bg-accent-600 cursor-pointer rounded-lg border disabled:cursor-not-allowed';
+  'border-accent-400 bg-accent-700 disabled:bg-accent-800 disabled:border-accent-600 hover:border-accent-200 hover:bg-accent-600 cursor-pointer rounded-lg border disabled:cursor-not-allowed text-light-500';
 
 export const buttonVariants: Record<ButtonVariants, string> = {
   default: defaultClassName,
