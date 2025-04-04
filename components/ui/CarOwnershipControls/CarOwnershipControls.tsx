40a5a17dc815d2d490b6bb66ca44e90b5c3591b6
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-04 09:40:50 +0200

    refactor(CarOwnershipControls): use IconButton

--- components/ui/CarOwnershipControls/CarOwnershipControls.tsx
+++ components/ui/CarOwnershipControls/CarOwnershipControls.tsx
@@ -6,9 +6,9 @@ import { UserMinusIcon } from '@/components/decorative/icons/UserMinusIcon';
 import { UserPlusIcon } from '@/components/decorative/icons/UserPlusIcon';
 
 import { AddCarOwnershipForm } from '../AddCarOwnershipForm/AddCarOwnershipForm';
-import { Button } from '../Button/Button';
 import { DialogModal, DialogModalRef } from '../DialogModal/DialogModal';
 import { GrantCarPrimaryOwnershipForm } from '../GrantPrimaryOwnershipForm/GrantPrimaryOwnershipForm';
+import { IconButton } from '../IconButton/IconButton';
 import {
   RemoveCarOwnershipForm,
   RemoveCarOwnershipFormRef,
@@ -32,7 +32,34 @@ export function CarOwnershipControls({
   const grantPrimaryOwnershipFormModalRef = useRef<DialogModalRef>(null);
 
   return (
-    <>
+    <div className="m-5 flex justify-end gap-5">
+      <IconButton
+        className="group"
+        disabled={!isCurrentUserPrimaryOwner}
+        title="grant primary ownership"
+        onClick={() => grantPrimaryOwnershipFormModalRef.current?.showModal()}
+      >
+        <ChangeKeyIcon className="group-disabled:stroke-light-800 h-full w-full stroke-3" />
+      </IconButton>
+      <IconButton
+        className="group"
+        disabled={
+          !removeCarOwnershipFormMethods.formState.isDirty &&
+          !removeCarOwnershipFormMethods.formState.isSubmitting
+        }
+        title="remove selected owners"
+        onClick={() => removeCarOwnershipFormModalRef.current?.showModal()}
+      >
+        <UserMinusIcon className="group-disabled:stroke-light-800 h-full w-full stroke-[0.5]" />
+      </IconButton>
+      <IconButton
+        className="group"
+        disabled={!isCurrentUserPrimaryOwner}
+        title="add owner"
+        onClick={() => newCarOwnerFormModalRef.current?.showModal()}
+      >
+        <UserPlusIcon className="group-disabled:stroke-light-800 h-full w-full stroke-[0.5]" />
+      </IconButton>
       <DialogModal ref={grantPrimaryOwnershipFormModalRef}>
         <GrantCarPrimaryOwnershipForm
           carId={carId}
@@ -63,38 +90,6 @@ export function CarOwnershipControls({
           onSubmit={() => newCarOwnerFormModalRef.current?.closeModal()}
         />
       </DialogModal>
-      <div className="m-5 flex justify-end gap-5">
-        <Button
-          className="border-accent-500 bg-accent-800 disabled:border-accent-700 disabled:bg-accent-900 disabled:text-light-800 cursor-pointer p-1.5"
-          disabled={!isCurrentUserPrimaryOwner}
-          title="Grant primary ownership"
-          onClick={() => grantPrimaryOwnershipFormModalRef.current?.showModal()}
-        >
-          <ChangeKeyIcon className="stroke-light-500 mx-2 h-full w-full stroke-7" />
-          <span className="sr-only">Grant primary ownership</span>
-        </Button>
-        <Button
-          className="border-accent-500 bg-accent-800 disabled:border-accent-700 disabled:bg-accent-900 disabled:text-light-800 cursor-pointer p-1.5"
-          disabled={
-            !removeCarOwnershipFormMethods.formState.isDirty &&
-            !removeCarOwnershipFormMethods.formState.isSubmitting
-          }
-          title="Remove selected owners"
-          onClick={() => removeCarOwnershipFormModalRef.current?.showModal()}
-        >
-          <UserMinusIcon className="stroke-light-500 mx-2 h-full w-full" />
-          <span className="sr-only">Remove selected owners</span>
-        </Button>
-        <Button
-          className="border-accent-500 bg-accent-800 disabled:border-accent-700 disabled:bg-accent-900 disabled:text-light-800 cursor-pointer p-1.5"
-          disabled={!isCurrentUserPrimaryOwner}
-          title="Add owner"
-          onClick={() => newCarOwnerFormModalRef.current?.showModal()}
-        >
-          <UserPlusIcon className="stroke-light-500 mx-2 h-full w-full" />
-          <span className="sr-only">Add owner</span>
-        </Button>
-      </div>
-    </>
+    </div>
   );
 }
