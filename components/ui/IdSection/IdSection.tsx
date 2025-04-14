ba9f39bc87b82a5037e1ff823d1e676dc611ee53
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-14 15:24:26 +0200

    refactor(IdSection): use DashboardSection & IdClipboardInput

--- components/ui/IdSection/IdSection.tsx
+++ components/ui/IdSection/IdSection.tsx
@@ -1,4 +1,5 @@
-import { SettingsSection } from '../SettingsSection/SettingsSection';
+import { DashboardSection } from '../DashboardSection/DashboardSection';
+import { IdClipboardInput } from '../IdClipboardInput/IdClipboardInput';
 
 type IdSectionProps = {
   id?: string;
@@ -6,30 +7,16 @@ type IdSectionProps = {
 
 export function IdSection({ id }: IdSectionProps) {
   return (
-    <SettingsSection headingText="ID">
-      <div className="text-sm">
-        <p
-          className="border-alpha-grey-300 my-4 cursor-pointer rounded-md border py-2 text-center whitespace-pre-wrap"
-          onClick={async () => {
-            if (!id) return;
-
-            const clipboardItemData = {
-              'text/plain': id,
-            };
-
-            const clipboardItem = new ClipboardItem(clipboardItemData);
-
-            await navigator.clipboard.write([clipboardItem]);
-          }}
-        >
-          {id || ' '}
-        </p>
-        <p>This ID uniquely identifies your profile.</p>
-        <p className="text-alpha-grey-700">
-          You can share it with another users to manage cars ownerships. Click
-          on it to automatically copy it to your clipboard.
-        </p>
-      </div>
-    </SettingsSection>
+    <DashboardSection>
+      <DashboardSection.Heading>ID</DashboardSection.Heading>
+      <DashboardSection.Text>
+        This ID uniquely identifies your profile.
+      </DashboardSection.Text>
+      <DashboardSection.Subtext className="my-4">
+        You can share it with another users to manage cars ownerships. Click on
+        it to automatically copy it to your clipboard.
+      </DashboardSection.Subtext>
+      <IdClipboardInput id={id} />
+    </DashboardSection>
   );
 }
