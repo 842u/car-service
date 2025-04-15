41857c7f926ac355d71ead1be96397e5a19c2eb6
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-15 14:15:52 +0200

    refactor(AddCarForm): extract logic to custom hook

--- components/ui/CarForm/AddCarForm.tsx
+++ components/ui/CarForm/AddCarForm.tsx
@@ -1,49 +1,12 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useRef } from 'react';
-
-import { useToasts } from '@/hooks/useToasts';
-import { CarFormValues } from '@/schemas/zod/carFormSchema';
-import { handleCarFormSubmit } from '@/utils/supabase/tables/cars';
-import {
-  carsInfiniteAddOnError,
-  carsInfiniteAddOnMutate,
-} from '@/utils/tanstack/cars';
-import { queryKeys } from '@/utils/tanstack/keys';
-
-import { CarForm, CarFormRef } from './CarForm';
+import { CarForm } from './CarForm';
+import { useAddCarForm } from './useAddCarForm';
 
 type AddCarFormProps = {
   onSubmit?: () => void;
 };
 
 export function AddCarForm({ onSubmit }: AddCarFormProps) {
-  const carFormRef = useRef<CarFormRef>(null);
-
-  const { addToast } = useToasts();
-
-  const queryClient = useQueryClient();
-  const { mutate } = useMutation({
-    throwOnError: false,
-    mutationFn: (carFormData: CarFormValues) =>
-      handleCarFormSubmit(carFormData, null, 'POST'),
-    onMutate: (carFormData) =>
-      carsInfiniteAddOnMutate(
-        carFormData,
-        queryClient,
-        carFormRef.current?.inputImageUrl || null,
-      ),
-    onSuccess: () => addToast('Car added successfully.', 'success'),
-    onError: (error, _, context) =>
-      carsInfiniteAddOnError(error, context, queryClient, addToast),
-  });
-
-  const handleFormSubmit = (carFormData: CarFormValues) => {
-    onSubmit && onSubmit();
-    mutate(carFormData, {
-      onSettled: () =>
-        queryClient.invalidateQueries({ queryKey: queryKeys.infiniteCars }),
-    });
-  };
+  const { handleFormSubmit, carFormRef } = useAddCarForm({ onSubmit });
 
   return (
     <CarForm ref={carFormRef} title="Add a car" onSubmit={handleFormSubmit} />
