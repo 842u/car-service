c5567bc495412fbc587bc6701a92a992141ee2f3
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-15 14:55:02 +0200

    refactor(CarDeleteForm): extract logic to custom hook

--- components/ui/CarDeleteForm/CarDeleteForm.tsx
+++ components/ui/CarDeleteForm/CarDeleteForm.tsx
@@ -1,52 +1,13 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { Route } from 'next';
-import { useRouter } from 'next/navigation';
-import { SyntheticEvent } from 'react';
-
-import { useToasts } from '@/hooks/useToasts';
-import { deleteCar } from '@/utils/supabase/tables/cars';
-import {
-  carsInfiniteDeleteOnError,
-  carsInfiniteDeleteOnMutate,
-} from '@/utils/tanstack/cars';
-import { queryKeys } from '@/utils/tanstack/keys';
-
 import { SubmitButton } from '../SubmitButton/SubmitButton';
+import { useCarDeleteForm } from './useCarDeleteForm';
 
-type CarDeleteFormProps = {
+export type CarDeleteFormProps = {
   carId: string;
   onSubmit: () => void;
 };
 
 export function CarDeleteForm({ carId, onSubmit }: CarDeleteFormProps) {
-  const router = useRouter();
-
-  const { addToast } = useToasts();
-
-  const queryClient = useQueryClient();
-  const { mutate } = useMutation({
-    mutationKey: queryKeys.infiniteCars,
-    throwOnError: false,
-    mutationFn: () => deleteCar(carId),
-    onMutate: () => carsInfiniteDeleteOnMutate(carId, queryClient),
-    onSuccess: () => addToast('Successfully deleted a car.', 'success'),
-    onError: (error, _, context) => {
-      addToast(error.message, 'error');
-      carsInfiniteDeleteOnError(queryClient, context);
-    },
-  });
-
-  const handleFormSubmit = async (event: SyntheticEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    mutate(undefined, {
-      onSettled: () =>
-        queryClient.invalidateQueries({
-          queryKey: queryKeys.infiniteCars,
-        }),
-    });
-    router.replace('/dashboard/cars' satisfies Route);
-    onSubmit && onSubmit();
-  };
+  const { handleFormSubmit } = useCarDeleteForm({ carId, onSubmit });
 
   return (
     <form
