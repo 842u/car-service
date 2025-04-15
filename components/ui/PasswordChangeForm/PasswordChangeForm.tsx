8ccc30c75db148b187e53622017ba17bfbca83ac
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-15 14:11:17 +0200

    refactor(PasswordChangeForm): extract logic to custom hook

--- components/ui/PasswordChangeForm/PasswordChangeForm.tsx
+++ components/ui/PasswordChangeForm/PasswordChangeForm.tsx
@@ -1,69 +1,18 @@
 'use client';
 
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Route } from 'next';
-import { useEffect } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
-
-import { useToasts } from '@/hooks/useToasts';
-import {
-  passwordChangeFormSchema,
-  PasswordChangeFormValues,
-} from '@/schemas/zod/passwordChangeFormSchema';
-import { RouteHandlerResponse } from '@/types';
-
 import { Input } from '../Input/Input';
 import { SubmitButton } from '../SubmitButton/SubmitButton';
-
-const defaultPasswordChangeFormValues: PasswordChangeFormValues = {
-  password: '',
-  passwordConfirm: '',
-};
+import { usePasswordChangeForm } from './usePasswordChangeForm';
 
 export function PasswordChangeForm() {
-  const { addToast } = useToasts();
-
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors, isSubmitSuccessful, isValid, isSubmitting },
-  } = useForm<PasswordChangeFormValues>({
-    resolver: zodResolver(passwordChangeFormSchema),
-    mode: 'onTouched',
-    defaultValues: defaultPasswordChangeFormValues,
-  });
-
-  const submitHandler: SubmitHandler<PasswordChangeFormValues> = async (
-    data,
-  ) => {
-    const password = JSON.stringify(data);
-
-    const response = await fetch('/api/auth/password-change' satisfies Route, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: password,
-    });
-
-    const { data: responseData, error } =
-      (await response.json()) as RouteHandlerResponse;
-
-    error && addToast(error.message, 'error');
-
-    responseData && addToast('Your password has been changed.', 'success');
-  };
-
-  useEffect(() => {
-    isSubmitSuccessful && reset();
-  }, [isSubmitSuccessful, reset]);
+  const { handleFormSubmit, register, errors, isValid, isSubmitting } =
+    usePasswordChangeForm();
 
   return (
     <form
       aria-label="password change"
       className="flex flex-col"
-      onSubmit={handleSubmit(submitHandler)}
+      onSubmit={handleFormSubmit}
     >
       <Input
         errorMessage={errors.password?.message}
