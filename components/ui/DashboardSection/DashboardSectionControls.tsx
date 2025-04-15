28aee0d8a60b951ee53a150deaf195b76b9c753b
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-15 10:17:11 +0200

    refactor(DashboardSectionControls): handle className prop

--- components/ui/DashboardSection/DashboardSectionControls.tsx
+++ components/ui/DashboardSection/DashboardSectionControls.tsx
@@ -1,13 +1,15 @@
 import { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 type DashboardSectionControlsProps = ComponentProps<'div'>;
 
 export function DashboardSectionControls({
   children,
+  className,
   ...props
 }: DashboardSectionControlsProps) {
   return (
-    <div className="flex justify-end gap-5" {...props}>
+    <div className={twMerge('flex justify-end gap-5', className)} {...props}>
       {children}
     </div>
   );
