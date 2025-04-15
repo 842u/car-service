3d397f2eca717eb864585c8c3a4dc7b77737beb5
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-15 13:47:23 +0200

    refactor(PasswordResetForm): extract logic to custom hook

--- components/ui/PasswordResetForm/PasswordResetForm.tsx
+++ components/ui/PasswordResetForm/PasswordResetForm.tsx
@@ -1,59 +1,16 @@
 'use client';
 
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect } from 'react';
-import { useForm } from 'react-hook-form';
-
-import { useToasts } from '@/hooks/useToasts';
-import {
-  passwordResetFormSchema,
-  PasswordResetFormValues,
-} from '@/schemas/zod/passwordResetFormSchema';
-import { createClient } from '@/utils/supabase/client';
-
 import { Input } from '../Input/Input';
 import { SubmitButton } from '../SubmitButton/SubmitButton';
 import { TextSeparator } from '../TextSeparator/TextSeparator';
-
-const defaultPasswordResetFormValues = { email: '' };
+import { usePasswordResetForm } from './usePasswordResetForm';
 
 export function PasswordResetForm() {
-  const { addToast } = useToasts();
-
-  const {
-    register,
-    reset,
-    handleSubmit,
-    formState: { errors, isSubmitSuccessful, isValid, isSubmitting },
-  } = useForm<PasswordResetFormValues>({
-    resolver: zodResolver(passwordResetFormSchema),
-    mode: 'onTouched',
-    defaultValues: defaultPasswordResetFormValues,
-  });
-
-  const handleFormSubmit = async (formData: PasswordResetFormValues) => {
-    const supabase = createClient();
-
-    const { data, error } = await supabase.auth.resetPasswordForEmail(
-      formData.email,
-      { redirectTo: window.location.origin },
-    );
-
-    error && addToast(error.message, 'error');
-
-    data &&
-      addToast(
-        'Your password reset request has been received. Please check your email for further instructions.',
-        'success',
-      );
-  };
-
-  useEffect(() => {
-    isSubmitSuccessful && reset();
-  }, [isSubmitSuccessful, reset]);
+  const { handleFormSubmit, errors, register, isValid, isSubmitting } =
+    usePasswordResetForm();
 
   return (
-    <form className="flex flex-col" onSubmit={handleSubmit(handleFormSubmit)}>
+    <form className="flex flex-col" onSubmit={handleFormSubmit}>
       <Input
         errorMessage={errors.email?.message}
         label="Email"
