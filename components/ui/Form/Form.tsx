fee759f3782e4fe0f6367ec4b64c51a53370ed09
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-15 16:24:04 +0200

    feat(Form): create component

---
+++ components/ui/Form/Form.tsx
@@ -0,0 +1,22 @@
+import { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+import { FormVariants } from '@/types';
+import { formVariants } from '@/utils/tailwindcss/form';
+
+type FormProps = ComponentProps<'form'> & {
+  variant?: FormVariants;
+};
+
+export function Form({
+  children,
+  className,
+  variant = 'default',
+  ...props
+}: FormProps) {
+  return (
+    <form className={twMerge(formVariants[variant], className)} {...props}>
+      {children}
+    </form>
+  );
+}
