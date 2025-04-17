97bce8598d4c5572ac125f0753395a39f1e91fda
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-17 13:50:47 +0200

    refactor(GrantPrimaryOwnershipForm): use Form

--- components/ui/GrantPrimaryOwnershipForm/GrantPrimaryOwnershipForm.tsx
+++ components/ui/GrantPrimaryOwnershipForm/GrantPrimaryOwnershipForm.tsx
@@ -1,6 +1,5 @@
 import { Button } from '../Button/Button';
-import { Input } from '../Input/Input';
-import { SubmitButton } from '../SubmitButton/SubmitButton';
+import { Form } from '../Form/Form';
 import { useGrantPrimaryOwnershipForm } from './useGrantPrimaryOwnershipForm';
 
 export type GrantCarPrimaryOwnershipFormProps = {
@@ -23,37 +22,34 @@ export function GrantCarPrimaryOwnershipForm({
   } = useGrantPrimaryOwnershipForm({ carId, onSubmit });
 
   return (
-    <form
-      className="border-accent-200 dark:border-accent-300 bg-light-500 dark:bg-dark-500 rounded-xl border-2 p-10 md:max-w-lg"
-      onSubmit={handleFormSubmit}
-    >
-      <h2>Grant primary ownership</h2>
-      <div className="bg-alpha-grey-200 my-4 h-[1px] w-full" />
-      <Input
-        required
-        errorMessage={errors.userId?.message}
-        label="User ID"
-        maxLength={36}
-        minLength={36}
-        name="userId"
-        register={register}
-        type="text"
-      />
-      <p className="text-warning-500 dark:text-warning-300">
-        Granting primary ownership to someone else will revoke your current
-        primary ownership status and the privileges that come with it.
-      </p>
-      <div className="mt-5 flex justify-end gap-5">
+    <Form className="gap-4" variant="raw" onSubmit={handleFormSubmit}>
+      <Form.InputWrapper>
+        <Form.Input
+          required
+          errorMessage={errors.userId?.message}
+          label="User ID"
+          maxLength={36}
+          minLength={36}
+          name="userId"
+          register={register}
+          type="text"
+        />
+        <p className="text-warning-500 dark:text-warning-300">
+          Granting primary ownership to someone else will revoke your current
+          primary ownership status and the privileges that come with it.
+        </p>
+      </Form.InputWrapper>
+      <Form.Controls>
         <Button disabled={!isDirty} onClick={handleFormReset}>
           Reset
         </Button>
-        <SubmitButton
+        <Form.ButtonSubmit
           disabled={!isValid || isSubmitting}
           isSubmitting={isSubmitting}
         >
           Save
-        </SubmitButton>
-      </div>
-    </form>
+        </Form.ButtonSubmit>
+      </Form.Controls>
+    </Form>
   );
 }
