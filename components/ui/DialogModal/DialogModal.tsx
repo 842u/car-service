eef09efb704a5e5341387db07ce3382a61eb8b97
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-17 12:32:25 +0200

    style(DialogModal): adjust width

--- components/ui/DialogModal/DialogModal.tsx
+++ components/ui/DialogModal/DialogModal.tsx
@@ -46,7 +46,7 @@ export function DialogModal({
   return (
     <dialog
       ref={dialogRef}
-      className="bg-light-500 dark:bg-dark-500 border-accent-200 dark:border-accent-300 fixed m-auto w-full rounded-md border p-4 backdrop:backdrop-blur-xs md:w-auto"
+      className="bg-light-500 dark:bg-dark-500 border-accent-200 dark:border-accent-300 fixed m-auto w-full rounded-md border p-4 backdrop:backdrop-blur-xs md:w-fit"
       onClick={closeModal}
       {...props}
     >
