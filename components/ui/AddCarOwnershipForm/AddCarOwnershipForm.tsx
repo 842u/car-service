a9d9920e78be02388348da82cbd01a6911a05940
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-17 13:54:58 +0200

    refactor(AddCarOwnershipForm): use Form

--- components/ui/AddCarOwnershipForm/AddCarOwnershipForm.tsx
+++ components/ui/AddCarOwnershipForm/AddCarOwnershipForm.tsx
@@ -1,6 +1,5 @@
 import { Button } from '../Button/Button';
-import { Input } from '../Input/Input';
-import { SubmitButton } from '../SubmitButton/SubmitButton';
+import { Form } from '../Form/Form';
 import { useAddCarOwnershipForm } from './useAddCarOwnershipForm';
 
 export type AddCarOwnershipFormProps = {
@@ -23,33 +22,30 @@ export function AddCarOwnershipForm({
   } = useAddCarOwnershipForm({ carId, onSubmit });
 
   return (
-    <form
-      className="border-accent-200 dark:border-accent-300 bg-light-500 dark:bg-dark-500 rounded-xl border-2 p-10"
-      onSubmit={handleFormSubmit}
-    >
-      <h2>Add new car owner</h2>
-      <div className="bg-alpha-grey-200 my-4 h-[1px] w-full" />
-      <Input
-        required
-        errorMessage={errors.userId?.message}
-        label="User ID"
-        maxLength={36}
-        minLength={36}
-        name="userId"
-        register={register}
-        type="text"
-      />
-      <div className="mt-5 flex justify-end gap-5">
+    <Form className="gap-4" variant="raw" onSubmit={handleFormSubmit}>
+      <Form.InputWrapper>
+        <Form.Input
+          required
+          errorMessage={errors.userId?.message}
+          label="User ID"
+          maxLength={36}
+          minLength={36}
+          name="userId"
+          register={register}
+          type="text"
+        />
+      </Form.InputWrapper>
+      <Form.Controls>
         <Button disabled={!isDirty} onClick={handleFormReset}>
           Reset
         </Button>
-        <SubmitButton
+        <Form.ButtonSubmit
           disabled={!isValid || isSubmitting}
           isSubmitting={isSubmitting}
         >
           Save
-        </SubmitButton>
-      </div>
-    </form>
+        </Form.ButtonSubmit>
+      </Form.Controls>
+    </Form>
   );
 }
