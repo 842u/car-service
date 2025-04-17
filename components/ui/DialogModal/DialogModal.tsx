d13859dd3237f8bcaae1f5d39f0e0512db6c9beb
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-17 10:18:39 +0200

    style(DialogModal): adjust backgrand and borders

--- components/ui/DialogModal/DialogModal.tsx
+++ components/ui/DialogModal/DialogModal.tsx
@@ -35,18 +35,18 @@ export function DialogModal({ children, ref, ...props }: DialogModalProps) {
 
   return (
     <dialog
-      {...props}
       ref={dialogRef}
-      className="fixed m-auto bg-transparent backdrop:backdrop-blur-xs"
+      className="bg-light-500 dark:bg-dark-500 border-accent-200 dark:border-accent-300 fixed m-auto w-full rounded-md border-2 p-4 backdrop:backdrop-blur-xs"
       onClick={closeModal}
+      {...props}
     >
       <div className="absolute top-0 right-0 m-4">
-        <IconButton className="h-10 p-1" title="close" onClick={closeModal}>
+        <IconButton className="p-1" title="close" onClick={closeModal}>
           <XCircleIcon className="stroke-dark-500 dark:stroke-light-500 h-full w-full stroke-2" />
         </IconButton>
       </div>
       <div
-        onClick={(event: SyntheticEvent) => {
+        onClick={(event: SyntheticEvent<HTMLDivElement>) => {
           event.stopPropagation();
         }}
       >
