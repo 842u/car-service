4d3a0c3cb7b83c4378dc25a5b082f8829044d5b8
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-23 13:22:21 +0200

    feat(useMouseTiltAnimation): extract custom hook

--- components/ui/Card/Card.tsx
+++ components/ui/Card/Card.tsx
@@ -1,94 +1,38 @@
 'use client';
 
-import {
-  HTMLMotionProps,
-  motion,
-  useMotionTemplate,
-  useMotionValue,
-  useSpring,
-  useTransform,
-} from 'motion/react';
-import { useEffect, useRef, useState } from 'react';
+import { HTMLMotionProps, motion } from 'motion/react';
 import { twMerge } from 'tailwind-merge';
 
-type CardProps = HTMLMotionProps<'div'> & {
-  rotationDirection?: 'push' | 'pull';
-  rotationFactor?: number;
-  transformPerspectivePixels?: number;
-};
+import {
+  useMouseTiltAnimation,
+  UseMouseTiltAnimationOptions,
+} from '@/hooks/useMouseTiltAnimation';
+
+type CardProps = HTMLMotionProps<'div'> & UseMouseTiltAnimationOptions;
 
 export function Card({
-  rotationDirection = 'push',
-  rotationFactor = 5,
-  transformPerspectivePixels = 1000,
+  rotationDirection,
+  rotationFactor,
+  transformPerspectivePixels,
   className,
   children,
   ...props
 }: CardProps) {
-  const [canAnimate, setCanAnimate] = useState(false);
-
-  const ref = useRef<HTMLDivElement>(null);
-
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
-
-  const xSpring = useSpring(x);
-  const ySpring = useSpring(y);
-
-  const rotateX = useTransform(
-    ySpring,
-    [-0.5, 0.5],
-    rotationDirection === 'push'
-      ? [rotationFactor, -rotationFactor]
-      : [-rotationFactor, rotationFactor],
-  );
-  const rotateY = useTransform(
-    xSpring,
-    [-0.5, 0.5],
-    rotationDirection === 'push'
-      ? [-rotationFactor, rotationFactor]
-      : [rotationFactor, -rotationFactor],
-  );
-
-  const transform = useMotionTemplate`perspective(${transformPerspectivePixels}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-
-  useEffect(() => setCanAnimate(true), []);
-
-  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (!ref.current) return;
-
-    const rect = ref.current.getBoundingClientRect();
-
-    const mouseXCoordinate = event.clientX - rect.left;
-    const mouseYCoordinate = event.clientY - rect.top;
-
-    const mouseXNormalized = mouseXCoordinate / rect.width - 0.5;
-    const mouseYNormalized = mouseYCoordinate / rect.height - 0.5;
-
-    x.set(mouseXNormalized);
-    y.set(mouseYNormalized);
-  };
-
-  const handleMouseLeave = () => {
-    x.set(0);
-    y.set(0);
-  };
+  const { elementRef, handleMouseLeave, handleMouseMove, style } =
+    useMouseTiltAnimation<HTMLDivElement>({
+      rotationDirection,
+      rotationFactor,
+      transformPerspectivePixels,
+    });
 
   return (
     <motion.div
-      ref={ref}
+      ref={elementRef}
       className={twMerge(
         'border-alpha-grey-300 hover:border-accent-300 rounded-md border p-4 shadow-lg drop-shadow-lg transition-colors duration-700',
         className,
       )}
-      style={
-        canAnimate
-          ? {
-              transformStyle: 'preserve-3d',
-              transform,
-            }
-          : undefined
-      }
+      style={style}
       onMouseLeave={handleMouseLeave}
       onMouseMove={handleMouseMove}
       {...props}
