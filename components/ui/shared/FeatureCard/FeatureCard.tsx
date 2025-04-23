0ecb1b9cdf8fe1af169f11b5ecf8b473430565b7
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-23 15:33:48 +0200

    feat(FeatureCardHeading): create component

--- components/ui/shared/FeatureCard/FeatureCard.tsx
+++ components/ui/shared/FeatureCard/FeatureCard.tsx
@@ -1,14 +1,19 @@
+import { ReactNode } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 import { Card, CardProps } from '../base/Card/Card';
+import { FeatureCardHeading } from './FeatureCardHeading';
 import { FeatureCardIcon } from './FeatureCardIcon';
 
 export function FeatureCard({ className, children, ...props }: CardProps) {
   return (
     <Card className={twMerge('@container h-96 w-full', className)} {...props}>
-      {children}
+      <div className="flex flex-col items-center justify-start gap-4 @sm:items-start @sm:text-left">
+        {children as ReactNode}
+      </div>
     </Card>
   );
 }
 
 FeatureCard.Icon = FeatureCardIcon;
+FeatureCard.Heading = FeatureCardHeading;
