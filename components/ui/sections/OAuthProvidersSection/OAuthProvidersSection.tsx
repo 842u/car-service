2d325ef24bba829a3b7059bbb44059d759d3a8d8
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-04-24 10:49:56 +0200

    feat(OAuthButton): create component (#76)
    * feat(OAuthButton): create component
    
    * refactor(OAuthProvidersSection): use OAuthButton

--- components/ui/sections/OAuthProvidersSection/OAuthProvidersSection.tsx
+++ components/ui/sections/OAuthProvidersSection/OAuthProvidersSection.tsx
@@ -1,55 +1,22 @@
-'use client';
-
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentProps } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-import { GitHubIcon } from '@/components/decorative/icons/GitHubIcon';
-import { GoogleIcon } from '@/components/decorative/icons/GoogleIcon';
-import { useToasts } from '@/hooks/useToasts';
-import { signInWithOAuthHandler } from '@/utils/supabase/general';
+import { OAuthButton } from '../../shared/OAuthButton/OAuthButton';
 
-import { Button } from '../../shared/base/Button/Button';
-
-type OAuthProvidersSectionProps = ComponentPropsWithoutRef<'section'>;
+type OAuthProvidersSectionProps = ComponentProps<'section'>;
 
 export function OAuthProvidersSection({
   className,
   ...props
 }: OAuthProvidersSectionProps) {
-  const { addToast } = useToasts();
-
-  const gitHubButtonClickHandler = async () => {
-    const { error } = await signInWithOAuthHandler('github');
-
-    error && addToast(error.message, 'error');
-  };
-
-  const googleButtonClickHandler = async () => {
-    const { error } = await signInWithOAuthHandler('google');
-
-    error && addToast(error.message, 'error');
-  };
-
   return (
     <section
       aria-label="OAuth Providers"
       className={twMerge('flex flex-col gap-4', className)}
       {...props}
     >
-      <Button
-        className="flex items-center justify-center gap-2 py-1.5"
-        onClick={gitHubButtonClickHandler}
-      >
-        <GitHubIcon className="fill-dark-500 dark:fill-light-500 stroke-dark-500 dark:stroke-light-500 h-full" />
-        <span>Continue with GitHub</span>
-      </Button>
-      <Button
-        className="flex items-center justify-center gap-2 py-1.5"
-        onClick={googleButtonClickHandler}
-      >
-        <GoogleIcon className="fill-dark-500 dark:fill-light-500 stroke-dark-500 dark:stroke-light-500 h-full" />
-        <span>Continue with Google</span>
-      </Button>
+      <OAuthButton provider="github" />
+      <OAuthButton provider="google" />
     </section>
   );
 }
