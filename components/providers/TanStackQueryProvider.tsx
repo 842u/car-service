a243c2a82415dbfca59f02d6f493a162582f7e6e
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-05-05 17:29:32 +0200

    feat(TanStackQueryProvider): add testing query client config

--- components/providers/TanStackQueryProvider.tsx
+++ components/providers/TanStackQueryProvider.tsx
@@ -1,8 +1,19 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  QueryClient,
+  QueryClientConfig,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode, useState } from 'react';
+
+const queryClientOptions: QueryClientConfig | undefined =
+  process.env.NODE_ENV === 'test' || process.env.CI
+    ? {
+        defaultOptions: { queries: { gcTime: Infinity, retry: false } },
+      }
+    : undefined;
 
 type TanStackQueryProviderProps = {
   children: ReactNode;
@@ -11,7 +22,7 @@ type TanStackQueryProviderProps = {
 export function TanStackQueryProvider({
   children,
 }: TanStackQueryProviderProps) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient(queryClientOptions));
 
   return (
     <QueryClientProvider client={queryClient}>
