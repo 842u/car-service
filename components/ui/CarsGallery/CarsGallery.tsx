c4bded7d1040fbdfaf8a9acb5b04608364a2a099
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-05-05 13:00:30 +0200

    refactor(CarsGallery): extract logic to custom hook

--- components/ui/CarsGallery/CarsGallery.tsx
+++ components/ui/CarsGallery/CarsGallery.tsx
@@ -1,76 +1,22 @@
-import {
-  useInfiniteQuery,
-  useIsMutating,
-  useQueryClient,
-} from '@tanstack/react-query';
-import { useEffect, useRef } from 'react';
-
-import { useToasts } from '@/hooks/useToasts';
-import { getCarsByPage } from '@/utils/supabase/tables/cars';
-import { queryKeys } from '@/utils/tanstack/keys';
-
 import { Spinner } from '../../decorative/Spinner/Spinner';
 import { CarCard } from '../shared/CarCard/CarCard';
+import { useCarsGallery } from './useCarsGallery';
 
 export function CarsGallery() {
-  const intersectionTargetRef = useRef<HTMLDivElement>(null);
-
-  const { addToast } = useToasts();
-
-  const carsInfiniteIsMutating = useIsMutating({
-    mutationKey: queryKeys.infiniteCars,
-  });
-  const queryClient = useQueryClient();
   const {
     data,
     error,
-    hasNextPage,
-    isPending,
     isError,
+    isPending,
+    hasNoCars,
     isSuccess,
-    isFetching,
+    intersectionTargetRef,
     isFetchingNextPage,
-    fetchNextPage,
-  } = useInfiniteQuery({
-    throwOnError: false,
-    enabled: !carsInfiniteIsMutating,
-    queryKey: queryKeys.infiniteCars,
-    queryFn: async ({ pageParam }) => {
-      const { data, nextPageParam } = await getCarsByPage({ pageParam });
+  } = useCarsGallery();
 
-      data.map((car) => queryClient.setQueryData(['cars', car.id], car));
-
-      return { data, nextPageParam };
-    },
-    initialPageParam: 0,
-    getNextPageParam: (lastPage) => lastPage.nextPageParam,
-  });
-
-  useEffect(() => {
-    if (!intersectionTargetRef.current || !hasNextPage) return;
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting) {
-          isSuccess && !isFetching && !isFetchingNextPage && fetchNextPage();
-        }
-      },
-      { threshold: 0.5 },
-    );
-
-    observer.observe(intersectionTargetRef.current);
-
-    return () => observer.disconnect();
-  }, [fetchNextPage, hasNextPage, isFetchingNextPage, isFetching, isSuccess]);
-
-  useEffect(() => {
-    isError && addToast(error.message, 'error');
-  }, [isError, addToast, error]);
-
-  const hasNoCars = isSuccess && data.pages[0].data.length === 0;
   return (
     <section aria-label="user's cars">
-      {isError && <p>{error.message}</p>}
+      {isError && <p>{error?.message}</p>}
       {isPending && (
         <Spinner className="stroke-accent-400 fill-accent-400 h-16 md:h-20 lg:h-24" />
       )}
@@ -84,7 +30,7 @@ export function CarsGallery() {
       )}
       {isSuccess && (
         <div className="relative flex flex-col gap-5 py-5 md:flex-row md:flex-wrap md:justify-center lg:max-w-[1920px]">
-          {data.pages.map((page) => {
+          {data?.pages.map((page) => {
             return page.data.map(
               (car) => car && <CarCard key={car.id} car={car} />,
             );
---
+++ components/ui/CarsGallery/useCarsGallery.ts
@@ -0,0 +1,81 @@
+import {
+  useInfiniteQuery,
+  useIsMutating,
+  useQueryClient,
+} from '@tanstack/react-query';
+import { useEffect, useRef } from 'react';
+
+import { useToasts } from '@/hooks/useToasts';
+import { getCarsByPage } from '@/utils/supabase/tables/cars';
+import { queryKeys } from '@/utils/tanstack/keys';
+
+export function useCarsGallery() {
+  const intersectionTargetRef = useRef<HTMLDivElement>(null);
+
+  const { addToast } = useToasts();
+
+  const carsInfiniteIsMutating = useIsMutating({
+    mutationKey: queryKeys.infiniteCars,
+  });
+
+  const queryClient = useQueryClient();
+
+  const {
+    data,
+    error,
+    hasNextPage,
+    isPending,
+    isError,
+    isSuccess,
+    isFetching,
+    isFetchingNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery({
+    throwOnError: false,
+    enabled: !carsInfiniteIsMutating,
+    queryKey: queryKeys.infiniteCars,
+    queryFn: async ({ pageParam }) => {
+      const { data, nextPageParam } = await getCarsByPage({ pageParam });
+
+      data.map((car) => queryClient.setQueryData(['cars', car.id], car));
+
+      return { data, nextPageParam };
+    },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextPageParam,
+  });
+
+  useEffect(() => {
+    if (!intersectionTargetRef.current || !hasNextPage) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          isSuccess && !isFetching && !isFetchingNextPage && fetchNextPage();
+        }
+      },
+      { threshold: 0.5 },
+    );
+
+    observer.observe(intersectionTargetRef.current);
+
+    return () => observer.disconnect();
+  }, [fetchNextPage, hasNextPage, isFetchingNextPage, isFetching, isSuccess]);
+
+  useEffect(() => {
+    isError && addToast(error.message, 'error');
+  }, [isError, addToast, error]);
+
+  const hasNoCars = isSuccess && data.pages[0].data.length === 0;
+
+  return {
+    isError,
+    isPending,
+    isSuccess,
+    error,
+    hasNoCars,
+    data,
+    intersectionTargetRef,
+    isFetchingNextPage,
+  };
+}
