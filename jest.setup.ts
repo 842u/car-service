55627694c964e96a0e992821c45a0b7e62359189
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-05-08 16:27:14 +0200

    test(jest): mock crypto

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,3 +3,4 @@ import 'whatwg-fetch';
 import '@/utils/jest/mocks/IntersectionObserver';
 import '@/utils/jest/mocks/createObjectURL';
 import '@/utils/jest/mocks/revokeObjectURL';
+import '@/utils/jest/mocks/crypto';
