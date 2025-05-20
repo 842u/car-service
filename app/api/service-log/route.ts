a1b09b10c71c38d0da45e7d9a8800f2ca0942258
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-05-20 17:25:33 +0200

    feat(api/service-log): create POST route handler

---
+++ app/api/service-log/route.ts
@@ -0,0 +1,146 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { ZodError } from 'zod';
+
+import {
+  carServiceLogAddFormSchema,
+  CarServiceLogAddFormValues,
+} from '@/schemas/zod/carServiceLogAddFormSchema';
+import { RouteHandlerResponse } from '@/types';
+import { createClient } from '@/utils/supabase/server';
+
+export type ServiceLogPostRouteHandlerRequest = {
+  formData: CarServiceLogAddFormValues;
+  car_id: string;
+};
+
+export type ServiceLogRouteHandlerResponse = {
+  id: string;
+};
+
+export const maxDuration = 10;
+
+export async function POST(request: NextRequest) {
+  if (request.headers.get('content-type') !== 'application/json')
+    return NextResponse.json<RouteHandlerResponse>(
+      {
+        error: {
+          message: "Invalid content type. Expected 'application/json'.",
+        },
+        data: null,
+      },
+      { status: 415 },
+    );
+
+  const { formData, car_id } =
+    (await request.json()) as Partial<ServiceLogPostRouteHandlerRequest>;
+
+  if (!formData) {
+    return NextResponse.json<RouteHandlerResponse>(
+      {
+        error: {
+          message: 'Missing form data in request body.',
+        },
+        data: null,
+      },
+      { status: 400 },
+    );
+  }
+
+  if (!car_id) {
+    return NextResponse.json<RouteHandlerResponse>(
+      {
+        error: {
+          message: "Missing 'car_id' in request body.",
+        },
+        data: null,
+      },
+      { status: 400 },
+    );
+  }
+
+  try {
+    carServiceLogAddFormSchema.parse(formData);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return NextResponse.json<RouteHandlerResponse>(
+        {
+          error: {
+            message: `Validation error: ${error.issues.map((issueError) => `${issueError.message}\n`)}`,
+          },
+          data: null,
+        },
+        { status: 400 },
+      );
+    }
+
+    if (error instanceof Error) {
+      return NextResponse.json<RouteHandlerResponse>(
+        {
+          error: {
+            message: `Validation error: ${error.message}.`,
+          },
+          data: null,
+        },
+        { status: 400 },
+      );
+    }
+
+    return NextResponse.json<RouteHandlerResponse>(
+      {
+        error: {
+          message: 'Validation error: Unknown error occurred.',
+        },
+        data: null,
+      },
+      { status: 400 },
+    );
+  }
+
+  const supabase = await createClient();
+  const { data: userData, error: userError } = await supabase.auth.getUser();
+
+  if (userError) {
+    return NextResponse.json<RouteHandlerResponse>(
+      {
+        error: {
+          message: 'Failed to retrieve authenticated user.',
+        },
+        data: null,
+      },
+      { status: 502 },
+    );
+  }
+
+  const { data: serviceLogData, error: serviceLogError } = await supabase
+    .from('service_logs')
+    .insert({
+      ...formData,
+      service_date: formData.service_date.toString(),
+      created_by: userData.user.id,
+      car_id,
+    })
+    .select('id')
+    .single();
+
+  if (serviceLogError) {
+    return NextResponse.json<RouteHandlerResponse>(
+      {
+        error: {
+          message: 'Failed to insert service log entry.',
+        },
+        data: null,
+      },
+      { status: 502 },
+    );
+  }
+
+  return NextResponse.json<
+    RouteHandlerResponse<ServiceLogRouteHandlerResponse>
+  >(
+    {
+      data: { id: serviceLogData.id },
+      error: null,
+    },
+    { status: 201 },
+  );
+}
