58ca1fa5dedcbd18adb16e81d09bcb51601b7f46
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-05-21 10:40:28 +0200

    refactor(api/service-log): cleanup

--- app/api/service-log/route.ts
+++ app/api/service-log/route.ts
@@ -1,11 +1,11 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 import { ZodError } from 'zod';
 
 import {
   carServiceLogAddFormSchema,
   CarServiceLogAddFormValues,
 } from '@/schemas/zod/carServiceLogAddFormSchema';
-import { RouteHandlerResponse } from '@/types';
+import { dataResponse, errorResponse } from '@/utils/next/routeHandlers';
 import { createClient } from '@/utils/supabase/server';
 
 export type ServiceLogPostRouteHandlerRequest = {
@@ -20,16 +20,12 @@ export type ServiceLogRouteHandlerResponse = {
 export const maxDuration = 10;
 
 export async function POST(request: NextRequest) {
-  if (request.headers.get('content-type') !== 'application/json')
-    return NextResponse.json<RouteHandlerResponse>(
-      {
-        error: {
-          message: "Invalid content type. Expected 'application/json'.",
-        },
-        data: null,
-      },
-      { status: 415 },
+  if (request.headers.get('content-type') !== 'application/json') {
+    return errorResponse(
+      "Invalid content type. Expected 'application/json'.",
+      415,
     );
+  }
 
   let formData: CarServiceLogAddFormValues | undefined;
   let car_id: string | undefined;
@@ -38,92 +34,39 @@ export async function POST(request: NextRequest) {
     ({ formData, car_id } =
       (await request.json()) as Partial<ServiceLogPostRouteHandlerRequest>);
   } catch (_) {
-    return NextResponse.json<RouteHandlerResponse>(
-      {
-        error: {
-          message: 'Invalid JSON.',
-        },
-        data: null,
-      },
-      { status: 400 },
-    );
+    return errorResponse('Invalid JSON.', 400);
   }
 
   if (!formData) {
-    return NextResponse.json<RouteHandlerResponse>(
-      {
-        error: {
-          message: 'Missing form data in request body.',
-        },
-        data: null,
-      },
-      { status: 400 },
-    );
+    return errorResponse('Missing form data in request body.', 400);
   }
 
   if (!car_id) {
-    return NextResponse.json<RouteHandlerResponse>(
-      {
-        error: {
-          message: "Missing 'car_id' in request body.",
-        },
-        data: null,
-      },
-      { status: 400 },
-    );
+    return errorResponse("Missing 'car_id' in request body.", 400);
   }
 
   try {
     carServiceLogAddFormSchema.parse(formData);
   } catch (error) {
     if (error instanceof ZodError) {
-      return NextResponse.json<RouteHandlerResponse>(
-        {
-          error: {
-            message: `Validation error: ${error.issues.map((issueError) => `${issueError.message}\n`)}`,
-          },
-          data: null,
-        },
-        { status: 400 },
+      return errorResponse(
+        `Validation error: ${error.issues.map((issueError) => `${issueError.message}\n`)}`,
+        400,
       );
     }
 
     if (error instanceof Error) {
-      return NextResponse.json<RouteHandlerResponse>(
-        {
-          error: {
-            message: `Validation error: ${error.message}.`,
-          },
-          data: null,
-        },
-        { status: 400 },
-      );
+      return errorResponse(`Validation error: ${error.message}.`, 400);
     }
 
-    return NextResponse.json<RouteHandlerResponse>(
-      {
-        error: {
-          message: 'Validation error: Unknown error occurred.',
-        },
-        data: null,
-      },
-      { status: 400 },
-    );
+    return errorResponse('Validation error: Unknown error occurred.', 400);
   }
 
   const supabase = await createClient();
   const { data: userData, error: userError } = await supabase.auth.getUser();
 
   if (userError) {
-    return NextResponse.json<RouteHandlerResponse>(
-      {
-        error: {
-          message: 'Failed to retrieve authenticated user.',
-        },
-        data: null,
-      },
-      { status: 502 },
-    );
+    return errorResponse('Failed to retrieve authenticated user.', 502);
   }
 
   const { data: serviceLogData, error: serviceLogError } = await supabase
@@ -138,24 +81,11 @@ export async function POST(request: NextRequest) {
     .single();
 
   if (serviceLogError) {
-    return NextResponse.json<RouteHandlerResponse>(
-      {
-        error: {
-          message: 'Failed to insert service log entry.',
-        },
-        data: null,
-      },
-      { status: 502 },
-    );
+    return errorResponse('Failed to insert service log entry.', 502);
   }
 
-  return NextResponse.json<
-    RouteHandlerResponse<ServiceLogRouteHandlerResponse>
-  >(
-    {
-      data: { id: serviceLogData.id },
-      error: null,
-    },
-    { status: 201 },
+  return dataResponse<ServiceLogRouteHandlerResponse>(
+    { id: serviceLogData.id },
+    201,
   );
 }
