83a7bbd2a79fdcf4c0a30b5d8c46fee95b3c4bed
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-05-21 14:28:31 +0200

    refactor(carServiceLogAddFormSchema): adjust schema

--- schemas/zod/carServiceLogFormSchema.ts
+++ schemas/zod/carServiceLogFormSchema.ts
@@ -8,8 +8,8 @@ import {
 
 import {
   carMileageSchema,
-  MIN_CAR_INSURANCE_EXPIRATION_DATE,
-  MIN_CAR_INSURANCE_EXPIRATION_DATE_MESSAGE,
+  MIN_CAR_PRODUCTION_YEAR_VALUE,
+  MIN_CAR_PRODUCTION_YEAR_VALUE_MESSAGE,
 } from './common';
 
 type CarServiceLogFormSchemaShape = Omit<
@@ -42,22 +42,35 @@ const serviceCostSchema = z
   })
   .nonnegative('Service cost must be a positive number.');
 
-const serviceDateSchema = z.coerce
-  .date({
-    required_error: 'Service date is required.',
-    invalid_type_error: 'Service date must be a date.',
-  })
-  .min(
-    new Date(MIN_CAR_INSURANCE_EXPIRATION_DATE),
-    MIN_CAR_INSURANCE_EXPIRATION_DATE_MESSAGE,
-  );
+const serviceDateSchema = z.string().superRefine((value, context) => {
+  const date = new Date(value);
+
+  if (Number.isNaN(date.valueOf())) {
+    context.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: 'Invalid date format.',
+      fatal: true,
+    });
+
+    return z.NEVER;
+  }
+
+  const year = date.getUTCFullYear();
+
+  if (year < MIN_CAR_PRODUCTION_YEAR_VALUE) {
+    context.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: MIN_CAR_PRODUCTION_YEAR_VALUE_MESSAGE,
+    });
+  }
+});
 
 export const carServiceLogFormSchema = z.object({
   service_date: serviceDateSchema,
-  mileage: carMileageSchema,
   category: serviceCategorySchema,
-  notes: serviceNotesSchema.optional().or(z.literal('')),
-  service_cost: serviceCostSchema.nullable().optional().or(z.nan()),
+  mileage: carMileageSchema.nullable().or(z.nan()),
+  notes: serviceNotesSchema.nullable().or(z.literal('')),
+  service_cost: serviceCostSchema.nullable().or(z.nan()),
 } satisfies CarServiceLogFormSchemaShape);
 
 export type CarServiceLogFormValues = z.infer<typeof carServiceLogFormSchema>;
