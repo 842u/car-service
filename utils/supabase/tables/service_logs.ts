fc6a6158a50e9602c6b6c4bbd3ed403a37dffd61
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-05-28 11:11:10 +0200

    feat(utils/supabase): create deleteServiceLogById()

--- utils/supabase/tables/service_logs.ts
+++ utils/supabase/tables/service_logs.ts
@@ -12,3 +12,24 @@ export async function getServiceLogsByCarId(carId: string) {
 
   return data;
 }
+
+export async function deleteServiceLogById(id: string) {
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.getUser();
+
+  if (error) throw new Error(error.message);
+
+  const { data, error: serviceLogError } = await supabase
+    .from('service_logs')
+    .delete()
+    .eq('id', id)
+    .select('id')
+    .single();
+
+  if (serviceLogError) throw new Error(serviceLogError.message);
+
+  if (!data) throw new Error('No service log was deleted.');
+
+  return data;
+}
