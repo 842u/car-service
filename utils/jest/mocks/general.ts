3b79f3474f4056f1368df5ae6c8f1d1b0d97ed10
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-06-04 15:43:33 +0200

    test(CarIdentitySection): adjust tests

---
+++ utils/jest/mocks/general.ts
@@ -0,0 +1,22 @@
+import { Car } from '@/types';
+
+export const MOCK_CAR: Car = {
+  id: crypto.randomUUID(),
+  custom_name: 'mock car',
+  additional_fuel_type: null,
+  brand: null,
+  created_at: null,
+  created_by: null,
+  drive_type: null,
+  engine_capacity: null,
+  fuel_type: null,
+  image_url: null,
+  insurance_expiration: null,
+  license_plates: null,
+  mileage: null,
+  model: null,
+  production_year: null,
+  technical_inspection_expiration: null,
+  transmission_type: null,
+  vin: null,
+};
