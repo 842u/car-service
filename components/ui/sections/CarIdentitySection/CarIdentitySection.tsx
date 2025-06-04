f1605a9c951b6e086afe6279fcef520224f6ecc1
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-06-04 15:43:00 +0200

    refactor(CarIdentitySection): simplify with useCarContext

--- components/ui/sections/CarIdentitySection/CarIdentitySection.tsx
+++ components/ui/sections/CarIdentitySection/CarIdentitySection.tsx
@@ -1,28 +1,20 @@
-import { Spinner } from '../../../decorative/Spinner/Spinner';
+'use client';
+
+import { useCarContext } from '@/hooks/useCarContext';
+
 import { CarImage } from '../../images/CarImage/CarImage';
 import { DashboardSection } from '../../shared/DashboardSection/DashboardSection';
 
-type CarIdentitySectionProps = {
-  name?: string;
-  imageUrl?: string | null;
-  isPending?: boolean;
-};
+export function CarIdentitySection() {
+  const car = useCarContext();
 
-export function CarIdentitySection({
-  imageUrl,
-  name,
-  isPending,
-}: CarIdentitySectionProps) {
   return (
     <DashboardSection className="flex flex-col-reverse items-center gap-5 md:flex-row">
       <div className="w-full max-w-md overflow-hidden rounded-lg md:max-w-xs md:basis-1/4">
-        {isPending && (
-          <Spinner className="stroke-accent-400 fill-accent-400 h-full w-full p-20 md:p-10" />
-        )}
-        {!isPending && <CarImage src={imageUrl} />}
+        <CarImage src={car.image_url} />
       </div>
       <h1 className="grow text-center text-3xl break-all whitespace-pre-wrap lg:break-normal">
-        {name || ' '}
+        {car.custom_name}
       </h1>
     </DashboardSection>
   );
