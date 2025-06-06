65510f56432799f64596937e95fd72e2ae76854a
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-06-06 10:19:35 +0200

    style(NavBar): incrase z index

--- components/ui/shared/base/NavBar/NavBar.tsx
+++ components/ui/shared/base/NavBar/NavBar.tsx
@@ -10,7 +10,7 @@ export function NavBar({ children, className }: NavBarProps) {
   return (
     <header
       className={twMerge(
-        'fixed top-0 z-10 flex h-16 w-full justify-between px-2 py-2 md:px-4 lg:px-6',
+        'fixed top-0 z-50 flex h-16 w-full justify-between px-2 py-2 md:px-4 lg:px-6',
         className,
       )}
     >
