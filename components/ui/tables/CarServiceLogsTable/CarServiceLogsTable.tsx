b0881ef9f2f80a893f15727012168dce6303f753
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-06-11 17:19:26 +0200

    refactor(CarServiceLogsTable): provide options prop for Table

--- components/ui/tables/CarServiceLogsTable/CarServiceLogsTable.tsx
+++ components/ui/tables/CarServiceLogsTable/CarServiceLogsTable.tsx
@@ -14,22 +14,38 @@ const columnsHelper = createColumnHelper<ServiceLog>();
 
 const columns = [
   columnsHelper.accessor('service_date', {
-    meta: { label: 'Date' },
+    meta: {
+      label: 'Date',
+    },
+    enableSorting: true,
+  }),
+  columnsHelper.accessor('created_at', {
+    meta: {
+      label: 'created_at',
+    },
     enableSorting: true,
   }),
   columnsHelper.accessor('category', {
-    meta: { label: 'Category' },
+    meta: {
+      label: 'Category',
+    },
   }),
   columnsHelper.accessor('mileage', {
-    meta: { label: 'Mileage' },
+    meta: {
+      label: 'Mileage',
+    },
     enableSorting: true,
   }),
   columnsHelper.accessor('service_cost', {
-    meta: { label: 'Cost' },
+    meta: {
+      label: 'Cost',
+    },
     enableSorting: true,
   }),
   columnsHelper.accessor('notes', {
-    meta: { label: 'Notes' },
+    meta: {
+      label: 'Notes',
+    },
   }),
   columnsHelper.display({ id: 'actions', cell: () => 'action' }),
 ] as ColumnDef<ServiceLog>[];
@@ -37,7 +53,24 @@ const columns = [
 export function CarServiceLogsTable({ serviceLogs }: CarServiceLogsTableProps) {
   return (
     serviceLogs && (
-      <Table columns={columns} data={serviceLogs}>
+      <Table
+        columns={columns}
+        data={serviceLogs}
+        options={{
+          initialState: {
+            columnVisibility: {
+              created_at: false,
+            },
+            sorting: [
+              { id: 'service_date', desc: true },
+              { id: 'created_at', desc: true },
+            ],
+          },
+          meta: {
+            intrinsicSort: { id: 'created_at', desc: true },
+          },
+        }}
+      >
         <Table.Head />
         <Table.Body />
       </Table>
