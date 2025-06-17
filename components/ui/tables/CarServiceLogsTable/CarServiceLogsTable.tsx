39acf689acb1d1d5a45737e8f3b86afa718b7dde
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-06-17 14:23:54 +0200

    refactor(CarServiceLogsTable): enable created_by sorting

--- components/ui/tables/CarServiceLogsTable/CarServiceLogsTable.tsx
+++ components/ui/tables/CarServiceLogsTable/CarServiceLogsTable.tsx
@@ -78,6 +78,7 @@ export function CarServiceLogsTable({
           },
           {
             id: 'created_by',
+            enableSorting: true,
             meta: {
               label: 'Creator',
             },
