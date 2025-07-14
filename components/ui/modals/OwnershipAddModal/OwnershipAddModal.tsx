7317bcdca60025912dad3117ed220e968be308bc
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-07-14 16:39:31 +0200

    feat(OwnershipAddModal): create component

---
+++ components/ui/modals/OwnershipAddModal/OwnershipAddModal.tsx
@@ -0,0 +1,31 @@
+import { Ref } from 'react';
+
+import { TextSeparator } from '@/components/decorative/TextSeparator/TextSeparator';
+
+import { CarOwnershipAddForm } from '../../forms/CarOwnershipAddForm/CarOwnershipAddForm';
+import {
+  DialogModal,
+  DialogModalRef,
+} from '../../shared/base/DialogModal/DialogModal';
+
+type OwnershipAddModalProps = {
+  carId: string;
+  ref?: Ref<DialogModalRef | null>;
+  onSubmit?: () => void;
+};
+
+export function OwnershipAddModal({
+  carId,
+  ref,
+  onSubmit,
+}: OwnershipAddModalProps) {
+  return (
+    <DialogModal ref={ref}>
+      <DialogModal.Root>
+        <DialogModal.Heading>Add owner</DialogModal.Heading>
+        <TextSeparator className="my-4" />
+        <CarOwnershipAddForm carId={carId} onSubmit={onSubmit} />
+      </DialogModal.Root>
+    </DialogModal>
+  );
+}
