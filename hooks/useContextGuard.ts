5a552d201e18bbcb9a758be163032562e461f60f
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-07-15 14:42:36 +0200

    feat(useContextGuard): create hook

---
+++ hooks/useContextGuard.ts
@@ -0,0 +1,20 @@
+import { Context, use } from 'react';
+
+type UseContextGuardOptions<T> = {
+  context: Context<T | null>;
+  componentName: string;
+};
+
+export function useContextGuard<T>({
+  context,
+  componentName,
+}: UseContextGuardOptions<T>) {
+  const receivedContext = use(context);
+
+  if (!receivedContext)
+    throw new Error(
+      `${componentName} related components should be wrapped in ${componentName}.`,
+    );
+
+  return receivedContext;
+}
