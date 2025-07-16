a3ba14e260e85a4ca68465bf7568db57d4ab6258
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-07-16 12:19:05 +0200

    refactor(CarDetailsSection): extract section controls

--- components/ui/sections/CarDetailsSection/CarDetailsSection.tsx
+++ components/ui/sections/CarDetailsSection/CarDetailsSection.tsx
@@ -1,12 +1,8 @@
-import { useRef } from 'react';
-
 import { Car } from '@/types';
 
-import { CarEditButton } from '../../buttons/CarEditButton/CarEditButton';
-import { CarEditModal } from '../../modals/CarEditModal/CarEditModal';
-import { DialogModalRef } from '../../shared/base/DialogModal/DialogModal';
 import { DashboardSection } from '../../shared/DashboardSection/DashboardSection';
 import { CarDetailsTable } from '../../tables/CarDetailsTable/CarDetailsTable';
+import { CarDetailsSectionControls } from './CarDetailsSectionControls/CarDetailsSectionControls';
 
 export type CarDetailsSectionProps = {
   isCurrentUserPrimaryOwner: boolean;
@@ -17,29 +13,16 @@ export function CarDetailsSection({
   isCurrentUserPrimaryOwner,
   carData,
 }: CarDetailsSectionProps) {
-  const dialogRef = useRef<DialogModalRef>(null);
-
-  const handleCarEditButtonClick = () => dialogRef.current?.showModal();
-
-  const handleCarEditModalSubmit = () => dialogRef.current?.closeModal();
-
   return (
     <DashboardSection>
       <DashboardSection.Heading headingLevel="h2">
         Details
       </DashboardSection.Heading>
       <CarDetailsTable carData={carData} />
-      <DashboardSection.Controls>
-        <CarEditButton
-          disabled={!isCurrentUserPrimaryOwner}
-          onClick={handleCarEditButtonClick}
-        />
-        <CarEditModal
-          ref={dialogRef}
-          carData={carData}
-          onSubmit={handleCarEditModalSubmit}
-        />
-      </DashboardSection.Controls>
+      <CarDetailsSectionControls
+        carData={carData}
+        isCurrentUserPrimaryOwner={isCurrentUserPrimaryOwner}
+      />
     </DashboardSection>
   );
 }
---
+++ components/ui/sections/CarDetailsSection/CarDetailsSectionControls/CarDetailsSectionControls.tsx
@@ -0,0 +1,33 @@
+import { useRef } from 'react';
+
+import { CarEditButton } from '@/components/ui/buttons/CarEditButton/CarEditButton';
+import { CarEditModal } from '@/components/ui/modals/CarEditModal/CarEditModal';
+import { DialogModalRef } from '@/components/ui/shared/base/DialogModal/DialogModal';
+import { DashboardSection } from '@/components/ui/shared/DashboardSection/DashboardSection';
+
+import { CarDetailsSectionProps } from '../CarDetailsSection';
+
+export function CarDetailsSectionControls({
+  isCurrentUserPrimaryOwner,
+  carData,
+}: CarDetailsSectionProps) {
+  const dialogRef = useRef<DialogModalRef>(null);
+
+  const handleCarEditButtonClick = () => dialogRef.current?.showModal();
+
+  const handleCarEditModalSubmit = () => dialogRef.current?.closeModal();
+
+  return (
+    <DashboardSection.Controls>
+      <CarEditButton
+        disabled={!isCurrentUserPrimaryOwner}
+        onClick={handleCarEditButtonClick}
+      />
+      <CarEditModal
+        ref={dialogRef}
+        carData={carData}
+        onSubmit={handleCarEditModalSubmit}
+      />
+    </DashboardSection.Controls>
+  );
+}
