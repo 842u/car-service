e7e8ea9c8e6ba72724dbe0599186e2fef42c3d33
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-07-16 15:44:04 +0200

    refactor(CarServiceLogsTableActionsDropdown): extract content

---
+++ components/ui/tables/CarServiceLogsTable/CarServiceLogsTableActionsDropdown/CarServiceLogsTableActionsDropdown.tsx
@@ -0,0 +1,54 @@
+import { EllipsisIcon } from '@/components/decorative/icons/EllipsisIcon';
+import { ServiceLog } from '@/types';
+
+import { Dropdown } from '../../../shared/base/Dropdown/Dropdown';
+import { IconButton } from '../../../shared/IconButton/IconButton';
+import { CarServiceLogsTableActionsDropdownContent } from './CarServiceLogsTableActionsDropdownContent/CarServiceLogsTableActionsDropdownContent';
+
+type CarServiceLogsTableActionsDropdownProps = {
+  carId: string;
+  serviceLog: ServiceLog;
+  isCurrentUserPrimaryOwner: boolean;
+  userId?: string;
+  className?: string;
+  collisionDetectionRoot?: HTMLElement | null;
+};
+
+export function CarServiceLogsTableActionsDropdown({
+  carId,
+  userId,
+  serviceLog,
+  isCurrentUserPrimaryOwner,
+  collisionDetectionRoot,
+  className,
+}: CarServiceLogsTableActionsDropdownProps) {
+  const canTakeAction =
+    isCurrentUserPrimaryOwner || userId === serviceLog.created_by;
+
+  return (
+    <Dropdown
+      className={className}
+      collisionDetectionRoot={collisionDetectionRoot}
+    >
+      <Dropdown.Trigger>
+        {({ onClick, ref }) => (
+          <IconButton
+            ref={ref}
+            className="group"
+            disabled={!canTakeAction}
+            title="Actions"
+            variant="transparent"
+            onClick={onClick}
+          >
+            <EllipsisIcon className="fill-dark-500 dark:fill-light-500 group-disabled:dark:fill-alpha-grey-500 group-disabled:dark:stroke-alpha-grey-500 group-disabled:fill-alpha-grey-500 group-disabled:stroke-alpha-grey-500 w-full px-1" />
+          </IconButton>
+        )}
+      </Dropdown.Trigger>
+      <CarServiceLogsTableActionsDropdownContent
+        canTakeAction={canTakeAction}
+        carId={carId}
+        serviceLog={serviceLog}
+      />
+    </Dropdown>
+  );
+}
