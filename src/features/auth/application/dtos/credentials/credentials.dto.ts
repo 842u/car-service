644b04f6374f54f0811e8448818261515f383084
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-08-14 17:03:40 +0200

    feat(auth/credentials): create CredentialsDto

--- src/features/auth/application/dtos/credentials/credentials.dto.ts
+++ src/features/auth/application/dtos/credentials/credentials.dto.ts
@@ -1,12 +1,32 @@
 import { z } from 'zod';
 
+import { ValidationError } from '@/common/application/errors/validation';
+import { Result } from '@/common/interface/result/result';
+import { toValidationIssue } from '@/common/utils/zod';
+
 z.config({
   jitless: true,
 });
 
-export const credentialsDtoSchema = z.object({
+const credentialsDtoSchema = z.object({
   email: z.string(),
   password: z.string(),
 });
 
 export type CredentialsDto = z.infer<typeof credentialsDtoSchema>;
+
+export function validateCredentialsDto(data: unknown) {
+  const result = credentialsDtoSchema.safeParse(data);
+
+  if (!result.success) {
+    const { error } = result;
+
+    const issues = error.issues.map((issue) => toValidationIssue(issue));
+
+    return Result.fail(new ValidationError('Data validation failed.', issues));
+  }
+
+  const { data: resultData } = result;
+
+  return Result.ok(resultData);
+}
