36526f10a1fb9474dc74a6939269e3b2dc510225
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-09-05 16:08:37 +0200

    refactor(api/auth/password-change): use DependencyContainer

--- src/app/api/auth/password-change/route.ts
+++ src/app/api/auth/password-change/route.ts
@@ -5,13 +5,13 @@ import {
   errorApiResponse,
   successApiResponse,
 } from '@/common/interface/api/response.interface';
+import { dependencyContainer, DependencyTokens } from '@/dependency-container';
 import { Password } from '@/user/domain/user/value-objects/password/password';
 import {
   type PasswordChangeApiResponseData,
   type PasswordChangeApiResponseError,
 } from '@/user/interface/api/password-change.schema';
 import { passwordChangeContractValidator } from '@/user/interface/contracts/password-change.schema';
-import { createClient } from '@/utils/supabase/server';
 
 type PasswordChangeApiResponse = ApiResponse<
   PasswordChangeApiResponseData,
@@ -58,17 +58,24 @@ export async function PATCH(request: NextRequest): PasswordChangeApiResponse {
     return errorApiResponse({ message, issues }, 422);
   }
 
-  const password = passwordResult.data;
+  const password = passwordResult.data.value;
 
-  const { auth } = await createClient();
+  const authClient = await dependencyContainer.resolve(
+    DependencyTokens.AUTH_SERVER_CLIENT,
+  );
 
-  const { data, error } = await auth.updateUser({
-    password: password.value,
+  const updateResult = await authClient.updateUser({
+    attributes: { password },
   });
 
-  if (error) {
-    return errorApiResponse({ message: error.message }, 401);
+  if (!updateResult.success) {
+    const { message } = updateResult.error;
+    return errorApiResponse({ message }, 401);
   }
 
-  return successApiResponse({ id: data.user.id }, 200);
+  const {
+    user: { id },
+  } = updateResult.data;
+
+  return successApiResponse({ id }, 200);
 }
