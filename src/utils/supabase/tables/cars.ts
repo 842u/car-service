bea76a6c9d67b13c550fe9d2dfa49b85373aa8ec
Kamil Bazanow <kamil.bazanow@gmail.com>
2025-09-10 17:58:05 +0200

    refactor(utils/supabase/cars): remove handleCarFormSubmit

--- src/utils/supabase/tables/cars.ts
+++ src/utils/supabase/tables/cars.ts
@@ -1,10 +1,4 @@
-import type { Route } from 'next';
-
-import type { ApiCarRequestBody, ApiCarResponse } from '@/app/api/car/route';
-import type { RouteHandlerResponse } from '@/common/types';
 import { dependencyContainer, dependencyTokens } from '@/dependency-container';
-import type { CarFormValues } from '@/schemas/zod/carFormSchema';
-import { CAR_IMAGE_UPLOAD_ERROR_CAUSE, hashFile } from '@/utils/general';
 import { CARS_INFINITE_QUERY_PAGE_DATA_LIMIT } from '@/utils/tanstack/cars';
 
 export async function getCar(carId: string) {
@@ -72,66 +66,4 @@ export async function getCarsByPage({ pageParam }: { pageParam: number }) {
   const hasMoreCars = !(data.length < CARS_INFINITE_QUERY_PAGE_DATA_LIMIT);
 
   return { data, nextPageParam: hasMoreCars ? pageParam + 1 : null };
-}
-
-export async function handleCarFormSubmit(
-  formData: CarFormValues,
-  carId: string | null,
-  method: 'POST' | 'PATCH',
-) {
-  const { image, ...data } = formData;
-
-  const jsonDataToValidate = JSON.stringify({
-    carFormData: data,
-    carId,
-  } satisfies ApiCarRequestBody);
-
-  const url = new URL(window.location.origin);
-  url.pathname = '/api/car' satisfies Route;
-
-  let newCarResponse: Response | null = null;
-
-  try {
-    newCarResponse = await fetch(url, {
-      method: method,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: jsonDataToValidate,
-    });
-  } catch (error) {
-    if (error instanceof Error) throw new Error(error.message);
-  }
-
-  const { data: responseData, error } =
-    (await newCarResponse?.json()) as RouteHandlerResponse<ApiCarResponse>;
-
-  if (error) throw new Error(error.message);
-
-  if (image && responseData?.id) {
-    const hashedFile = await hashFile(image);
-
-    const storageClient = await dependencyContainer.resolve(
-      dependencyTokens.STORAGE_BROWSER_CLIENT,
-    );
-
-    const uploadPath = `${responseData.id}/${hashedFile}`;
-
-    const uploadResult = await storageClient.upload(
-      'cars_images',
-      uploadPath,
-      image,
-    );
-
-    if (!uploadResult.success) {
-      throw new Error(
-        'Car added successfully, but image upload failed. You can edit and upload the image in your car details.',
-        {
-          cause: CAR_IMAGE_UPLOAD_ERROR_CAUSE,
-        },
-      );
-    }
-  }
-
-  return responseData;
 }
