b0ceeaccaebce9d7d40e10aedca28de1215692e3
Kamil Bazanow <kamil.bazanow@gmail.com>
2025-09-11 12:16:15 +0200

    recaftor(user/interface/api): remove password change validator

--- src/modules/user/interface/api/password-change.schema.ts
+++ src/modules/user/interface/api/password-change.schema.ts
@@ -1,6 +1,5 @@
 import { z } from 'zod';
 
-import { ZodValidator } from '@/common/infrastructure/validation/zod-validator';
 import { createApiResponseSchema } from '@/common/interface/api/response.schema';
 
 z.config({
@@ -10,7 +9,7 @@ z.config({
 const passwordChangeApiResponseDataSchema = z.object({ id: z.string() });
 const passwordChangeApiResponseErrorSchema = z.object({ message: z.string() });
 
-const passwordChangeApiResponseSchema = createApiResponseSchema(
+export const passwordChangeApiResponseSchema = createApiResponseSchema(
   passwordChangeApiResponseDataSchema,
   passwordChangeApiResponseErrorSchema,
 );
@@ -22,8 +21,3 @@ export type PasswordChangeApiResponseData = z.infer<
 export type PasswordChangeApiResponseError = z.infer<
   typeof passwordChangeApiResponseErrorSchema
 >;
-
-export const passwordChangeApiResponseValidator = new ZodValidator(
-  passwordChangeApiResponseSchema,
-  'Invalid API response format.',
-);
