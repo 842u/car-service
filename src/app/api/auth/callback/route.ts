779e384f50a623ef0c2c2d79ee3920bd06f12cfe
Kamil Bazanow <kamil.bazanow@gmail.com>
2025-09-12 16:43:29 +0200

    refactor(api/auth/callback): use api handler

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,20 +1,20 @@
 import { type EmailOtpType } from '@supabase/supabase-js';
+import type { Route } from 'next';
 import type { NextRequest } from 'next/server';
 
-import {
-  errorApiResponse,
-  redirectApiResponse,
-} from '@/common/interface/api/response.interface';
 import { dependencyContainer, dependencyTokens } from '@/dependency-container';
 
 export const maxDuration = 10;
 
-const ON_SUCCESS_PATH = '/dashboard';
-
 export async function GET(request: NextRequest) {
+  const apiHandler = await dependencyContainer.resolve(
+    dependencyTokens.API_HANDLER,
+  );
+
   const requestUrl = request.nextUrl.clone();
   const { searchParams } = requestUrl;
-  const next = searchParams.get('next') ?? ON_SUCCESS_PATH;
+  const next = searchParams.get('next') ?? ('/dashboard' satisfies Route);
+  searchParams.delete('next');
 
   const authClient = await dependencyContainer.resolve(
     dependencyTokens.AUTH_SERVER_CLIENT,
@@ -22,34 +22,31 @@ export async function GET(request: NextRequest) {
 
   const token_hash = searchParams.get('token_hash');
   const type = searchParams.get('type') as EmailOtpType | null;
+  searchParams.delete('token_hash');
+  searchParams.delete('type');
 
   if (token_hash && type) {
     const otpResult = await authClient.verifyOtp({ type, token_hash });
 
-    searchParams.delete('token_hash');
-    searchParams.delete('type');
-    searchParams.delete('next');
-
     if (!otpResult.success) {
       const { message } = otpResult.error;
-      return errorApiResponse({ message }, 400);
+      return apiHandler.errorResponse({ message }, 400);
     }
   }
 
   const code = searchParams.get('code');
+  searchParams.delete('code');
 
   if (code) {
     const exchangeResult = await authClient.exchangeCodeForSession(code);
 
-    searchParams.delete('code');
-
     if (!exchangeResult.success) {
       const { message } = exchangeResult.error;
-      return errorApiResponse({ message }, 400);
+      return apiHandler.errorResponse({ message }, 400);
     }
   }
 
   requestUrl.pathname = next;
 
-  return redirectApiResponse(requestUrl, 307);
+  return apiHandler.redirectResponse(requestUrl, 307);
 }
