e8bdc554ccbf27b3f698c41de854fa038428f6ac
Kamil Bazanow <kamil.bazanow@gmail.com>
2025-09-24 11:52:53 +0200

    feat(common): create Mapper interface

---
+++ src/common/application/mapper/mapper.interface.ts
@@ -0,0 +1,10 @@
+import type { Result } from '@/common/application/result/result';
+
+export interface Mapper<TDomain, TDto, TPersistence> {
+  domainToDto(model: TDomain): TDto;
+  persistenceToDto(model: TPersistence): TDto;
+  domainToPersistence(model: TDomain): TPersistence;
+  dtoToPersistence(model: TDto): TPersistence;
+  dtoToDomain(model: TDto): Result<TDomain, unknown>;
+  persistenceToDomain(model: TPersistence): Result<TDomain, unknown>;
+}
