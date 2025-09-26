896fa9411594382e2789027cfe730b798d5e1193
Kamil Bazanow <kamil.bazanow@gmail.com>
2025-09-26 14:08:20 +0200

    feat(user): create SignInUserWithOAuthUseCase

---
+++ src/modules/user/application/use-cases/sign-in-with-o-auth.ts
@@ -0,0 +1,69 @@
+import { Result } from '@/common/application/result/result';
+import type { UseCase } from '@/common/application/use-case/use-case.interface';
+import type { SupabaseAuthClient } from '@/common/infrastructure/auth/supabase-auth-client';
+import type { UserMapper } from '@/user/application/mappers/user-mapper';
+import type { UserRepository } from '@/user/infrastructure/repositories/user-repository';
+
+type SignInWithOAuthContract = {
+  code: string;
+};
+
+type SignInWithOAuthUseCaseError = {
+  code: number;
+};
+
+export class SignInUserWithOAuthUseCase
+  implements UseCase<SignInWithOAuthContract, SignInWithOAuthUseCaseError>
+{
+  private readonly _authClient: SupabaseAuthClient;
+  private readonly _userRepository: UserRepository;
+  private readonly _userMapper: UserMapper;
+
+  constructor(
+    authClient: SupabaseAuthClient,
+    userRepository: UserRepository,
+    userMapper: UserMapper,
+  ) {
+    this._authClient = authClient;
+    this._userRepository = userRepository;
+    this._userMapper = userMapper;
+  }
+
+  async execute(contract: SignInWithOAuthContract) {
+    const { code } = contract;
+    /**
+     * Exchanging code for session will either retrieve an existing auth identity or create a new one.
+     */
+    const exchangeResult = await this._authClient.exchangeCodeForSession(code);
+
+    if (!exchangeResult.success) {
+      const { message, status } = exchangeResult.error;
+      return Result.fail({ message, code: status || 500 });
+    }
+
+    const { user: authIdentity } = exchangeResult.data;
+
+    const getUserResult = await this._userRepository.getById(authIdentity.id);
+
+    if (getUserResult.success) return Result.ok(getUserResult.data);
+
+    /**
+     * If there is no user, that means it's a first time sign-in, so create and store a new user.
+     */
+    const userResult = this._userMapper.authIdentityToDomain(authIdentity);
+
+    if (!userResult.success) {
+      const { message } = userResult.error;
+      return Result.fail({ message, code: 500 });
+    }
+
+    const storeUserResult = await this._userRepository.store(userResult.data);
+
+    if (!storeUserResult.success) {
+      const { message } = storeUserResult.error;
+      return Result.fail({ message, code: 500 });
+    }
+
+    return Result.ok(userResult.data);
+  }
+}
