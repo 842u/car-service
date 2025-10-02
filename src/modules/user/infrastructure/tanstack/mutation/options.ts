4288f7a4ebe726c654a2cabed277bced5fdbe46d
Kamil Bazanow <kamil.bazanow@gmail.com>
2025-10-02 13:58:38 +0200

    refactor(updateUserNameMutationOptions): use UserApiClient

--- src/modules/user/infrastructure/tanstack/mutation/options.ts
+++ src/modules/user/infrastructure/tanstack/mutation/options.ts
@@ -1,18 +1,27 @@
 import type { QueryClient } from '@tanstack/react-query';
 import { mutationOptions } from '@tanstack/react-query';
 
+import { dependencyContainer, dependencyTokens } from '@/di';
 import type { UserNameChangeContract } from '@/user/interface/contracts/name-change.schema';
-import { updateCurrentSessionProfile } from '@/utils/supabase/tables/profiles';
 import { profilesUpdateOnMutate } from '@/utils/tanstack/profiles';
 
 export const updateUserNameMutationOptions = (queryClient: QueryClient) =>
   mutationOptions({
     throwOnError: false,
-    mutationFn: (variables: UserNameChangeContract) =>
-      updateCurrentSessionProfile({
-        property: 'username',
-        value: variables.name.trim(),
-      }),
+    mutationFn: async (variables: UserNameChangeContract) => {
+      const userApiClient = await dependencyContainer.resolve(
+        dependencyTokens.USER_API_CLIENT,
+      );
+
+      const nameChangeResult = await userApiClient.nameChange(variables);
+
+      if (!nameChangeResult.success) {
+        const { message } = nameChangeResult.error;
+        throw new Error(message);
+      }
+
+      return nameChangeResult.data;
+    },
     onMutate: (variables) =>
       profilesUpdateOnMutate(
         queryClient,
