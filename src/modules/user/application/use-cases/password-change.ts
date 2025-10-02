88f47a9826abccae6ea2e1cf2aec8e585328071f
Kamil Bazanow <kamil.bazanow@gmail.com>
2025-10-02 11:50:54 +0200

    feat(user): create UserPasswordChangeUseCase

---
+++ src/modules/user/application/use-cases/password-change.ts
@@ -0,0 +1,60 @@
+import { Result } from '@/common/application/result/result';
+import type { UseCase } from '@/common/application/use-case/use-case.interface';
+import type { SupabaseAuthClient } from '@/common/infrastructure/auth/supabase-auth-client';
+import { Password } from '@/user/domain/user/value-objects/password/password';
+import type { UserRepository } from '@/user/infrastructure/repositories/user-repository';
+import type { PasswordChangeContract } from '@/user/interface/contracts/password-change.schema';
+
+type UserPasswordChangeUseCaseError = { code: number };
+
+export class UserPasswordChangeUseCase
+  implements UseCase<PasswordChangeContract, UserPasswordChangeUseCaseError>
+{
+  private readonly _authClient: SupabaseAuthClient;
+  private readonly _userRepository: UserRepository;
+
+  constructor(authClient: SupabaseAuthClient, userRepository: UserRepository) {
+    this._authClient = authClient;
+    this._userRepository = userRepository;
+  }
+
+  async execute(contract: PasswordChangeContract) {
+    const { password: passwordDto, passwordConfirm } = contract;
+
+    if (passwordDto !== passwordConfirm) {
+      return Result.fail({ message: 'Passwords do not match.', code: 400 });
+    }
+
+    const passwordResult = Password.create(passwordDto);
+
+    if (!passwordResult.success) {
+      const { message } = passwordResult.error;
+
+      return Result.fail({ message, code: 422 });
+    }
+
+    const password = passwordResult.data.value;
+
+    const updateResult = await this._authClient.updateUser({
+      attributes: { password },
+    });
+
+    if (!updateResult.success) {
+      const { message } = updateResult.error;
+      return Result.fail({ message, code: 401 });
+    }
+
+    const { user: authIdentity } = updateResult.data;
+
+    const getUserResult = await this._userRepository.getById(authIdentity.id);
+
+    if (!getUserResult.success) {
+      const { message } = getUserResult.error;
+      return Result.fail({ message, code: 500 });
+    }
+
+    const user = getUserResult.data;
+
+    return Result.ok(user);
+  }
+}
