ef59aaca32fa707c7380bc20c741076359b0ee17
Kamil Bazanow <kamil.bazanow@gmail.com>
2025-10-06 15:37:39 +0200

    chore(user/persistence-models): use singular dir name

--- src/modules/user/application/mapper/user-mapper.ts
+++ src/modules/user/application/mapper/user-mapper.ts
@@ -3,7 +3,7 @@ import type { User as AuthIdentity } from '@supabase/auth-js';
 import type { Mapper } from '@/common/application/mapper/mapper.interface';
 import { Result } from '@/common/application/result/result';
 import type { UserDto } from '@/user/application/dto/user-dto';
-import type { UserPersistence } from '@/user/application/persistence-models/user-persistence';
+import type { UserPersistence } from '@/user/application/persistence-model/user-persistence';
 import { User } from '@/user/domain/user/user';
 
 export class UserMapper implements Mapper<User, UserDto, UserPersistence> {
