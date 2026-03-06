42f0326581335f90f0914ab1b43870e334a45ead
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-03-06 18:37:03 +0100

    refactor(lib/getRequestOrigin): strip trailing slash

--- src/lib/http/get-request-origin.ts
+++ src/lib/http/get-request-origin.ts
@@ -2,5 +2,5 @@ export function getRequestOrigin(request: Request): string {
   const host =
     request.headers.get('x-forwarded-host') || request.headers.get('host');
   const protocol = request.headers.get('x-forwarded-proto') || 'https';
-  return `${protocol}://${host}`;
+  return `${protocol}://${host}`.replace(/\/$/, '');
 }
