925df385eb82bc111f4e0b994170a6c4c1b781d0
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-03-09 12:26:13 +0100

    refactor(lib/utils): remove unused functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,38 +1,4 @@
 export const CAR_IMAGE_UPLOAD_ERROR_CAUSE = 'image upload error';
-const DEFAULT_TIMEOUT = 9000;
-
-// Default vercel serverless function will timeout after 10s so promise should reject in less than that time.
-export async function promiseWithTimeout<T>(
-  promise: Promise<T>,
-  time = DEFAULT_TIMEOUT,
-  timeoutError = new Error(
-    'There was an error with the upstream service. Try again.',
-  ),
-) {
-  const timeoutPromise = new Promise((_, reject) => {
-    setTimeout(() => reject(new Error()), time);
-  });
-
-  return Promise.race([promise, timeoutPromise])
-    .then((result) => result as T)
-    .catch(() => {
-      throw timeoutError;
-    });
-}
-
-export function unslugify(slug: string, capitalize: boolean) {
-  let chunks = slug.trim().split('-');
-
-  if (capitalize) {
-    chunks = chunks.map((chunk) => {
-      const firstCharacter = chunk.at(0)?.toUpperCase();
-
-      return firstCharacter + chunk.slice(1);
-    });
-  }
-
-  return chunks.join(' ');
-}
 
 export async function hashFile(file: File) {
   const arrayBuffer = await file.arrayBuffer();
