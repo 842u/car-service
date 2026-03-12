82195d61da70e6b0072439fe8e12810c1992bd14
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-03-12 16:36:56 +0100

    refactor(DashboardPage): use TotalOwnershipsSection

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,29 @@
-import { DashboardMain } from '@/dashboard/ui/main/main';
+'use client';
 
-export default async function DashboardPage() {
+import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+
+import { TotalOwnershipsSection } from '@/car/ownership/ui/sections/total-ownerships/total-ownerships';
+import { useToasts } from '@/common/presentation/hook/use-toasts';
+import { DashboardMain } from '@/dashboard/ui/main/main';
+import { getSessionUserQueryOptions } from '@/user/infrastructure/tanstack/query/options';
+
+export default function DashboardPage() {
+  const { addToast } = useToasts();
+
+  const {
+    data: userData,
+    error: userError,
+    isError: userIsError,
+  } = useQuery(getSessionUserQueryOptions);
+
+  useEffect(() => {
+    userIsError && addToast(userError.message, 'error');
+  }, [userIsError, addToast, userError]);
+
   return (
     <DashboardMain>
-      <h1>DASHBOARD</h1>
+      <TotalOwnershipsSection ownerId={userData?.id || ''} />
     </DashboardMain>
   );
 }
