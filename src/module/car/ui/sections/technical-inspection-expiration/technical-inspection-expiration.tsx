6276103d6597b4d14475e5deb3a4c7cb70a76c12
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-03-16 13:51:56 +0100

    refactor(car/TechnicalInspectionExpirationSection): extract hook

--- src/module/car/ui/sections/technical-inspection-expiration/technical-inspection-expiration.tsx
+++ src/module/car/ui/sections/technical-inspection-expiration/technical-inspection-expiration.tsx
@@ -1,43 +1,29 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
-
+import { useTechnicalInspectionExpirationSection } from '@/car/ui/sections/technical-inspection-expiration/use-technical-inspection';
 import { DashboardSection } from '@/dashboard/ui/section/section';
-import { getCarsByPage } from '@/lib/supabase/tables/cars';
-import { queryKeys } from '@/lib/tanstack/keys';
+import { Spinner } from '@/ui/decorative/spinner/spinner';
 
 export function TechnicalInspectionExpirationSection() {
-  const { data } = useInfiniteQuery({
-    throwOnError: false,
-    queryKey: queryKeys.carsInfiniteByColumnOrder(
-      'technical_inspection_expiration',
-    ),
-    queryFn: async ({ pageParam }) => {
-      const { data, nextPageParam } = await getCarsByPage({
-        pageParam,
-        pageLimit: 3,
-        orderBy: { column: 'technical_inspection_expiration', ascending: true },
-      });
-
-      return { data, nextPageParam };
-    },
-    initialPageParam: 0,
-    getNextPageParam: (lastPage) => lastPage.nextPageParam,
-  });
+  const { data, isPending } = useTechnicalInspectionExpirationSection();
 
   return (
     <DashboardSection>
       <DashboardSection.Heading>
         Technical inspection expiration
       </DashboardSection.Heading>
-      {data?.pages.map((page) => (
-        <div key={crypto.randomUUID()}>
-          {page.data.map((car) => (
-            <div key={crypto.randomUUID()}>
-              <p>{car.id}</p>
-              <p>{car.technical_inspection_expiration}</p>
-            </div>
-          ))}
-        </div>
-      ))}
+      {isPending ? (
+        <Spinner className="stroke-accent-400 fill-accent-400 my-10 h-16 w-full" />
+      ) : (
+        data?.pages.map((page, pageIndex) => (
+          <div key={pageIndex}>
+            {page.data.map((car) => (
+              <div key={car.id}>
+                <p>{car.id}</p>
+                <p>{car.insurance_expiration}</p>
+              </div>
+            ))}
+          </div>
+        ))
+      )}
     </DashboardSection>
   );
 }
