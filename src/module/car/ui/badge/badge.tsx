adab5cb76f83b86a8ed95792d08dd8e66120b382
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-03-16 15:55:51 +0100

    feat(car): create CarBadge

---
+++ src/module/car/ui/badge/badge.tsx
@@ -0,0 +1,26 @@
+import { twMerge } from 'tailwind-merge';
+
+import { CarImage } from '@/car/ui/image/image';
+
+interface CarBadgeProps {
+  name: string;
+  imageUrl?: string | null;
+  className?: string;
+}
+
+export function CarBadge({ name, imageUrl, className }: CarBadgeProps) {
+  return (
+    <div
+      className={twMerge(
+        'flex flex-row items-center justify-center gap-2 overflow-auto',
+        className,
+      )}
+    >
+      <p className="overflow-auto">{name}</p>
+      <CarImage
+        className="border-alpha-grey-300 aspect-square h-full w-fit shrink-0 overflow-hidden rounded-full border"
+        src={imageUrl}
+      />
+    </div>
+  );
+}
