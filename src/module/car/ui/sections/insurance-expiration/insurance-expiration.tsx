cc9073fd67d0588d8e3b73c249a807a2fdd4a5cc
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-03-17 12:50:46 +0100

    refactor(car/InsuranceExpirationSection): use InsuranceExpirationTable

--- src/module/car/ui/sections/insurance-expiration/insurance-expiration.tsx
+++ src/module/car/ui/sections/insurance-expiration/insurance-expiration.tsx
@@ -1,27 +1,11 @@
-import { useInsuranceExpirationSection } from '@/car/ui/sections/insurance-expiration/use-insurance-expiration';
+import { InsuranceExpirationTable } from '@/car/ui/tables/insurance-expiration/insurance-expiration';
 import { DashboardSection } from '@/dashboard/ui/section/section';
-import { Spinner } from '@/ui/decorative/spinner/spinner';
 
 export function InsuranceExpirationSection() {
-  const { data, isPending } = useInsuranceExpirationSection();
-
   return (
     <DashboardSection>
       <DashboardSection.Heading>Insurance expiration</DashboardSection.Heading>
-      {isPending ? (
-        <Spinner className="stroke-accent-400 fill-accent-400 my-10 h-16 w-full" />
-      ) : (
-        data?.pages.map((page, pageIndex) => (
-          <div key={pageIndex}>
-            {page.data.map((car) => (
-              <div key={car.id}>
-                <p>{car.id}</p>
-                <p>{car.insurance_expiration}</p>
-              </div>
-            ))}
-          </div>
-        ))
-      )}
+      <InsuranceExpirationTable />
     </DashboardSection>
   );
 }
