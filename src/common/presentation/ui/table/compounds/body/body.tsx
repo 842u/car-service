36e8d08fbb4eda83b6d4d80677a555e7303d017b
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-03-17 14:49:34 +0100

    style(common/TableBody): add hover style for row

--- src/common/presentation/ui/table/compounds/body/body.tsx
+++ src/common/presentation/ui/table/compounds/body/body.tsx
@@ -18,7 +18,7 @@ export function TableBody({ lastRowRef }: TableBodyProps) {
         <tr
           key={row.id}
           ref={index === rows.length - 1 ? lastRowRef : null}
-          className="border-alpha-grey-200 text-dark-400 dark:text-light-600 border-b last-of-type:border-0"
+          className="border-alpha-grey-200 text-dark-400 dark:text-light-600 hover:bg-alpha-grey-100 border-b last-of-type:border-0"
         >
           {row.getVisibleCells().map((cell) => {
             return (
