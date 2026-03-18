2ea6e91d4c48c26f83825dfa8b9237f5e607c81a
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-03-18 13:50:10 +0100

    style(car/TotalOwnerships): setup for lg screens

--- src/module/car/ownership/ui/sections/total-ownerships/total-ownerships.tsx
+++ src/module/car/ownership/ui/sections/total-ownerships/total-ownerships.tsx
@@ -1,3 +1,5 @@
+import { twMerge } from 'tailwind-merge';
+
 import { useTotalOwnershipsSection } from '@/car/ownership/ui/sections/total-ownerships/use-total-ownerships';
 import { DashboardSection } from '@/dashboard/ui/section/section';
 import { Spinner } from '@/ui/decorative/spinner/spinner';
@@ -14,11 +16,13 @@ export function TotalOwnershipsSection({
   const { data, isPending } = useTotalOwnershipsSection({ ownerId });
 
   return (
-    <DashboardSection className={className}>
-      <DashboardSection.Heading headingLevel="h2">
+    <DashboardSection
+      className={twMerge('lg:flex lg:flex-col lg:justify-center', className)}
+    >
+      <DashboardSection.Heading className="lg:self-start" headingLevel="h2">
         Cars owned
       </DashboardSection.Heading>
-      <DashboardSection.Text className="text-center text-9xl">
+      <DashboardSection.Text className="text-center text-9xl lg:flex lg:grow lg:items-center lg:justify-center">
         {isPending ? (
           <Spinner className="stroke-accent-400 fill-accent-400 my-10 h-16 w-full" />
         ) : (
