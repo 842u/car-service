c422baca9837509bd1824bc7eb9e8712e8c2dd5b
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-03-18 12:34:04 +0100

    style(car/CostsSection): setup for tablet screens

--- src/module/car/service-log/ui/sections/costs/costs.tsx
+++ src/module/car/service-log/ui/sections/costs/costs.tsx
@@ -23,42 +23,44 @@ export function CostsSection() {
         Costs
       </DashboardSection.Heading>
 
-      <DashboardSection variant="raw">
-        <DashboardSection.Heading
-          className="text-alpha-grey-900"
-          headingLevel="h3"
-          withUnderline={false}
-        >
-          All time costs
-        </DashboardSection.Heading>
-        <DashboardSection.Text className="text-5xl">
-          {data?.totalCost ?? 0}
-        </DashboardSection.Text>
-      </DashboardSection>
+      <div className="md:flex">
+        <DashboardSection variant="raw">
+          <DashboardSection.Heading
+            className="text-alpha-grey-900 font-normal"
+            headingLevel="h3"
+            withUnderline={false}
+          >
+            All time costs
+          </DashboardSection.Heading>
+          <DashboardSection.Text className="text-5xl">
+            {data?.totalCost ?? 0}
+          </DashboardSection.Text>
+        </DashboardSection>
 
-      <DashboardSection variant="raw">
-        <DashboardSection.Heading
-          className="text-alpha-grey-900"
-          headingLevel="h3"
-          withUnderline={false}
-        >
-          Past year costs
-        </DashboardSection.Heading>
-        <DashboardSection.Text className="text-5xl">
-          {data?.yearToDateCost ?? 0}
-        </DashboardSection.Text>
-      </DashboardSection>
+        <DashboardSection variant="raw">
+          <DashboardSection.Heading
+            className="text-alpha-grey-900 font-normal"
+            headingLevel="h3"
+            withUnderline={false}
+          >
+            Past year costs
+          </DashboardSection.Heading>
+          <DashboardSection.Text className="text-5xl">
+            {data?.yearToDateCost ?? 0}
+          </DashboardSection.Text>
+        </DashboardSection>
+      </div>
 
       <DashboardSection className="flex flex-col gap-2" variant="raw">
         <DashboardSection.Heading
-          className="text-alpha-grey-900"
+          className="text-alpha-grey-900 font-normal"
           headingLevel="h3"
         >
           Custom period
         </DashboardSection.Heading>
 
-        <div>
-          <label>
+        <div className="md:flex md:gap-5">
+          <label className="md:w-full">
             <p className="my-2 text-xs">From</p>
             <input
               className={inputVariants.default}
@@ -68,7 +70,7 @@ export function CostsSection() {
             />
           </label>
 
-          <label>
+          <label className="md:w-full">
             <p className="my-2 text-xs">To</p>
             <input
               className={inputVariants.default}
