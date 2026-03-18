7fd949690a192e302c96f3ba2e61f0f063146216
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-03-18 13:59:34 +0100

    style(car/DateExpirationTableLegend): setup for md & lg screens

--- src/module/car/ui/tables/date-expiration/legend/legend.tsx
+++ src/module/car/ui/tables/date-expiration/legend/legend.tsx
@@ -4,7 +4,7 @@ import { ExclamationTriangleIcon } from '@/icons/exclamation-triangle';
 
 export function DateExpirationTableLegend() {
   return (
-    <div className="mt-5 flex justify-evenly gap-6 text-sm">
+    <div className="mt-5 flex justify-evenly gap-6 text-sm md:m-5 md:justify-start">
       <div className="flex items-center gap-1">
         <CheckCircleIcon className="stroke-success-500 h-4 stroke-2" />
         Valid
