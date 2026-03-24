6eba9798c93cb05a2a9c657a8e8a4a3e4a066055
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-03-24 17:22:03 +0100

    refactor(user/IdClipboardInput): improve ux

--- src/module/user/presentation/ui/id-clipboard-input/id-clipboard-input.tsx
+++ src/module/user/presentation/ui/id-clipboard-input/id-clipboard-input.tsx
@@ -1,51 +1,73 @@
-import type { SyntheticEvent } from 'react';
+import { useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 import { useToasts } from '@/common/presentation/hook/use-toasts';
+import { CheckIcon } from '@/icons/check';
+import { ClipboardIcon } from '@/icons/clipboard';
 import { inputVariants } from '@/lib/tailwindcss/input';
 
 type IdClipboardInputProps = {
   id?: string;
+  label?: string;
   className?: string;
+  variant?: keyof typeof inputVariants;
 };
 
-export function IdClipboardInput({ id, className }: IdClipboardInputProps) {
+export function IdClipboardInput({
+  id,
+  label = 'ID',
+  className,
+  variant = 'default',
+}: IdClipboardInputProps) {
   const { addToast } = useToasts();
+  const [copied, setCopied] = useState(false);
 
-  const handleInputClick = async (event: SyntheticEvent<HTMLInputElement>) => {
+  const handleCopy = async () => {
     if (!id) return;
 
-    event.currentTarget.select();
-
-    const clipboardItemData = {
-      'text/plain': id,
-    };
-
-    const clipboardItem = new ClipboardItem(clipboardItemData);
-
     try {
-      await navigator.clipboard.write([clipboardItem]);
+      await navigator.clipboard.writeText(id);
+      setCopied(true);
       addToast('ID copied.', 'success');
-    } catch (error) {
-      if (error instanceof Error) {
-        addToast('Writing to the clipboard is not allowed.', 'error');
-      }
+
+      setTimeout(() => setCopied(false), 2000);
+    } catch {
+      addToast('Clipboard not allowed.', 'error');
     }
   };
 
   return (
-    <input
-      readOnly
-      aria-label="current user ID"
-      className={twMerge(
-        inputVariants['default'],
-        'selection:bg-accent-500 cursor-pointer overflow-x-auto text-center text-xs md:w-72',
-        className,
+    <label className="selection:bg-accent-500 block max-w-72" title="Copy ID">
+      {label && (
+        <p>
+          <span className="text-xs">{label}</span>
+        </p>
       )}
-      defaultValue={id}
-      placeholder="..."
-      type="text"
-      onClick={handleInputClick}
-    />
+
+      <div
+        className={twMerge(
+          inputVariants[variant],
+          'my-1 flex items-center p-0',
+          'focus-within:border-accent-500 hover:border-accent-500 transition-colors',
+          className,
+        )}
+      >
+        <input
+          readOnly
+          className="text-alpha-grey-900 inline-block h-full w-full cursor-pointer truncate pl-3 text-xs outline-none"
+          placeholder="..."
+          value={id ?? ''}
+          onClick={handleCopy}
+        />
+
+        <div className="flex h-full cursor-pointer items-center justify-center p-2">
+          {copied ? (
+            <CheckIcon className="h-full w-full stroke-3" />
+          ) : (
+            <ClipboardIcon className="h-full w-full stroke-3" />
+          )}
+        </div>
+      </div>
+    </label>
   );
 }
