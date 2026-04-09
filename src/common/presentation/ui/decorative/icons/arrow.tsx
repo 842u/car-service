f378fe55de7814329c2563e20b7ceb459b65f066
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-04-09 12:08:30 +0200

    feat(common): create ArrowIcon

---
+++ src/common/presentation/ui/decorative/icons/arrow.tsx
@@ -0,0 +1,15 @@
+import type { SvgA11yProps } from '../svg-a11y/svg-a11y';
+import { SvgA11y } from '../svg-a11y/svg-a11y';
+
+export function ArrowIcon({ ...props }: SvgA11yProps) {
+  return (
+    <SvgA11y
+      {...props}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      viewBox="0 0 50 50"
+    >
+      <path d="M29.25 7 48 25.5m0 0L29.25 44M48 25.5H3" />
+    </SvgA11y>
+  );
+}
