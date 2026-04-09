102fa6ded088146dfa7ea497507fde2f8ea48898
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-04-09 13:28:28 +0200

    style(common/TableTextFilter): adjust colors

--- src/common/presentation/ui/table/compounds/text-filter/text-filter.tsx
+++ src/common/presentation/ui/table/compounds/text-filter/text-filter.tsx
@@ -1,14 +1,18 @@
+import { twMerge } from 'tailwind-merge';
+
 import { SearchIcon } from '@/icons/search';
 import { useTextFilter } from '@/ui/table/compounds/text-filter/use-text-filter';
 
 interface TableTextFilterProps {
   columnId: string;
   debounceDelay?: number;
+  className?: string;
 }
 
 export function TableTextFilter({
   columnId,
   debounceDelay,
+  className,
 }: TableTextFilterProps) {
   const { columnLabel, inputId, handleInputChange } = useTextFilter({
     columnId,
@@ -16,16 +20,21 @@ export function TableTextFilter({
   });
 
   return (
-    <label className="border-alpha-grey-200 bg-alpha-grey-50 focus-within:ring-accent-500 flex h-10 w-full items-center rounded-md border focus-within:ring-1">
+    <label
+      className={twMerge(
+        'border-alpha-grey-200 bg-alpha-grey-50 focus-within:ring-accent-500 flex h-10 w-full items-center rounded-md border focus-within:ring-1',
+        className,
+      )}
+    >
       <SearchIcon
         aria-hidden="true"
-        className="stroke-accent-500 dark:accent-accent-400 pointer-events-none ml-2 h-5 w-5 shrink-0 stroke-2"
+        className="stroke-accent-500 dark:accent-accent-400 pointer-events-none ml-2 h-5 w-5 shrink-0 stroke-3"
       />
 
       <span className="sr-only">Filter {columnLabel}</span>
 
       <input
-        className="ml-2 w-full outline-none"
+        className="placeholder:text-alpha-grey-900 ml-2 w-full outline-none"
         id={inputId}
         placeholder={`Search by ${columnLabel}`}
         type="text"
