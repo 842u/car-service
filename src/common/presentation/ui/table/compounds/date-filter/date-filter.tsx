701e2e7cb1991572868a91f10812f2c905254c35
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-04-09 12:56:33 +0200

    style(common/Table): restyle TableDateFilter

--- src/common/presentation/ui/table/compounds/date-filter/date-filter.tsx
+++ src/common/presentation/ui/table/compounds/date-filter/date-filter.tsx
@@ -1,4 +1,4 @@
-import { inputVariants } from '@/lib/tailwindcss/input';
+import { ArrowIcon } from '@/icons/arrow';
 import { useDateFilter } from '@/ui/table/compounds/date-filter/use-date-filter';
 
 interface TableDateFilterProps {
@@ -11,20 +11,24 @@ export function TableDateFilter({ columnId }: TableDateFilterProps) {
   });
 
   return (
-    <div className="my-4 flex flex-col gap-2 md:w-fit md:flex-row md:flex-wrap">
-      <label className="md:grow">
-        <p className="my-2 text-xs">From {columnLabel}</p>
+    <div className="border-alpha-grey-200 bg-alpha-grey-50 flex flex-col rounded-md border md:h-10 md:flex-row md:items-center md:justify-evenly">
+      <label className="hover:bg-alpha-grey-100 focus-within:ring-accent-500 m-2 cursor-pointer rounded-sm p-2 transition-colors duration-200 focus-within:ring-1 md:m-0">
+        <p className="sr-only">From {columnLabel}</p>
         <input
-          className={inputVariants.default}
+          className="h-full w-full cursor-pointer outline-none"
+          title={`From ${columnLabel}`}
           type="date"
           onChange={onFromDateChange}
         />
       </label>
 
-      <label className="md:grow">
-        <p className="my-2 text-xs">To {columnLabel}</p>
+      <ArrowIcon className="stroke-accent-500 mx-auto h-5 w-5 rotate-90 stroke-3 md:mx-0 md:rotate-0" />
+
+      <label className="hover:bg-alpha-grey-100 focus-within:ring-accent-500 m-2 cursor-pointer rounded-sm p-2 transition-colors duration-200 focus-within:ring-1 md:m-0">
+        <p className="sr-only">To {columnLabel}</p>
         <input
-          className={inputVariants.default}
+          className="h-full w-full cursor-pointer outline-none"
+          title={`To ${columnLabel}`}
           type="date"
           onChange={onToDateChange}
         />
