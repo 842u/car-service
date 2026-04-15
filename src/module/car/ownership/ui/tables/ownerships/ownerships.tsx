f9c532207b773fc6df02d6a7e96aa9cc110811fe
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-04-15 12:31:19 +0200

    feat(car/OwnershipsTable): add ID filter

--- src/module/car/ownership/ui/tables/ownerships/ownerships.tsx
+++ src/module/car/ownership/ui/tables/ownerships/ownerships.tsx
@@ -43,11 +43,14 @@ export function OwnershipsTable({
         },
       }}
     >
-      <Table.TextFilter className="lg:w-fit" columnId="user" />
+      <div className="flex flex-col gap-5 md:flex-row">
+        <Table.TextFilter className="lg:w-fit" columnId="user" />
+        <Table.TextFilter className="lg:w-fit" columnId="owner_id" />
+      </div>
       <Table.SortBreadcrumb className="mt-5" />
       <Table.Root
         ref={tableRef}
-        className="my-4 max-h-96 overflow-auto lg:max-h-52"
+        className="my-4 max-h-96 overflow-auto lg:max-h-60"
       >
         <caption className="sr-only">car ownerships</caption>
         <Table.Head />
