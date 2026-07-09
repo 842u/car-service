ae6c834cfa57b76521b804471984b56501b8a37c
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-07-09 12:31:57 +0200

    feat(car): add imageChange to CarApiClient and NextCarApiClient

--- src/module/car/presentation/api-client/car.ts
+++ src/module/car/presentation/api-client/car.ts
@@ -1,6 +1,7 @@
 import type { CarDto } from '@/car/application/dto/car';
 import type { AddCarApiRequest } from '@/car/interface/api/add.schema';
 import type { EditCarApiRequest } from '@/car/interface/api/edit.schema';
+import type { CarImageChangeApiRequest } from '@/car/interface/api/image-change.schema';
 import type { Result } from '@/common/application/result';
 
 type CarApiClientError = { message: string };
@@ -9,4 +10,7 @@ export interface CarApiClient {
   add(contract: AddCarApiRequest): Promise<Result<CarDto, CarApiClientError>>;
   edit(contract: EditCarApiRequest): Promise<Result<CarDto, CarApiClientError>>;
   remove(carId: string): Promise<Result<null, CarApiClientError>>;
+  imageChange(
+    contract: CarImageChangeApiRequest,
+  ): Promise<Result<CarDto, CarApiClientError>>;
 }
