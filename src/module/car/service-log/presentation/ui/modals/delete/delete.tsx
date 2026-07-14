b4cc174a88a6440b404de1bddcaa7ab7480dd3ef
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-07-14 12:25:49 +0200

    feat(service-log): add the Delete Service Log modal

---
+++ src/module/car/service-log/presentation/ui/modals/delete/delete.tsx
@@ -0,0 +1,38 @@
+import type { RefObject } from 'react';
+
+import { Button } from '@/ui/button/button';
+import { TextSeparator } from '@/ui/decorative/text-separator/text-separator';
+import type { DialogModalRef } from '@/ui/dialog-modal/dialog-modal';
+import { DialogModal } from '@/ui/dialog-modal/dialog-modal';
+
+type DeleteModalProps = {
+  canTakeAction: boolean;
+  ref?: RefObject<DialogModalRef | null>;
+  onCancel?: () => void;
+  onConfirm?: () => void;
+  onClose?: () => void;
+};
+
+export function DeleteModal({
+  canTakeAction,
+  ref,
+  onCancel,
+  onConfirm,
+  onClose,
+}: DeleteModalProps) {
+  return (
+    <DialogModal ref={ref}>
+      <DialogModal.Root onClose={onClose}>
+        <DialogModal.Heading>Delete service log</DialogModal.Heading>
+        <TextSeparator className="my-4" />
+        <p className="my-4">Are you sure you want to delete service log?</p>
+        <DialogModal.Controls>
+          <Button onClick={onCancel}>Cancel</Button>
+          <Button disabled={!canTakeAction} variant="error" onClick={onConfirm}>
+            Delete
+          </Button>
+        </DialogModal.Controls>
+      </DialogModal.Root>
+    </DialogModal>
+  );
+}
